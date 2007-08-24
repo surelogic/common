bc6b52a0105b39f4028a70f3a31cf745e7410e10
Tim Halloran <tim.halloran@surelogic.com>
2007-08-24 14:59:40 +0000

    Working
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@1205 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/logging/SLFormatter.java
@@ -0,0 +1,63 @@
+package com.surelogic.common.logging;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ * A concise format for SureLogic loggers.
+ */
+public final class SLFormatter extends Formatter {
+
+	private final static ThreadLocal<SimpleDateFormat> tl_format = new ThreadLocal<SimpleDateFormat>() {
+		@Override
+		protected SimpleDateFormat initialValue() {
+			return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		}
+	};
+
+	/**
+	 * Format the given log record and return the formatted string. The
+	 * resulting formatted String outputs single lines using the following
+	 * pattern:
+	 * <p>[<i>yyyy-MM-dd HH:mm:ss</i> <i>LEVEL</i>] <i>message</i> [<i>package.class.method()</i>
+	 * in <i>thread name</i>]
+	 * <p>
+	 * If the given log record contains an exception then the stack trace
+	 * reported by that exception is added to the above as subsequent lines.
+	 * <p>
+	 * The {@link #formatMessage(LogRecord)} convenience method is used to
+	 * localize and format the message field.
+	 * 
+	 * @param record
+	 *            the log record to be formatted.
+	 * @return the formatted log record.
+	 */
+	@Override
+	public String format(final LogRecord record) {
+		StringBuilder buf = new StringBuilder(1000);
+		buf.append("[").append(tl_format.get().format(new Date()));
+		String level = record.getLevel().getName();
+		int padding = 8 - level.length();
+		for (int i = 0; i < padding; i++)
+			buf.append(" ");
+		buf.append(record.getLevel().getName()).append("] ");
+
+		buf.append(formatMessage(record));
+
+		buf.append(" [").append(record.getSourceClassName()).append(".")
+				.append(record.getSourceMethodName()).append("() in ");
+		buf.append(Thread.currentThread().getName()).append("]");
+		buf.append(System.getProperty("line.separator"));
+		Throwable t = record.getThrown();
+		if (t != null) {
+			StringWriter sw = new StringWriter();
+			t.printStackTrace(new PrintWriter(sw));
+			buf.append(sw.toString());
+		}
+		return buf.toString();
+	}
+}
--- src/com/surelogic/common/logging/SLLogger.java
+++ src/com/surelogic/common/logging/SLLogger.java
@@ -1,48 +1,92 @@
 package com.surelogic.common.logging;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
 import java.util.logging.Handler;
-import java.util.logging.LogRecord;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class SLLogger extends Formatter {
-
-	public static final boolean f_useSLLogger = System
-			.getProperty("no-SLLogger") == null;
+/**
+ * A utility for obtaining SureLogic loggers.
+ */
+public class SLLogger {
+	public static void main(String[] args) {
+		Logger l = getLogger("foo");
+		l.info("test of info");
+		l.log(Level.WARNING, "test of warning", new IllegalArgumentException(
+				"my wacky message"));
+	}
 
 	/**
 	 * Everyone can reuse the same instance of this formatter because the
 	 * format() method uses no instance state.
 	 */
-	public static final SLLogger f_formatter;
+	public static final AtomicReference<SLFormatter> f_formatter = new AtomicReference<SLFormatter>(
+			new SLFormatter());
 
 	/**
 	 * A simple cache of loggers we have already configured.
 	 */
-	public static final Map<String, Logger> f_nameToLogger;
+	public static final Map<String, Logger> f_nameToLogger = new HashMap<String, Logger>();
 
-	static {
-		if (f_useSLLogger) {
-			f_formatter = new SLLogger();
-			f_nameToLogger = new HashMap<String, Logger>();
-		} else {
-			f_formatter = null;
-			f_nameToLogger = null;
+	/**
+	 * A list of the handlers we manage for logging.
+	 */
+	public static final List<Handler> f_handlers = new ArrayList<Handler>();
+
+	/**
+	 * Adds all the handlers to the given logger. It is a precondition that a
+	 * lock on this class be held when invoking this method.
+	 * 
+	 * @param logger
+	 *            the logger to add handlers to.
+	 */
+	private static void addAllHandlersTo(final Logger logger) {
+		for (Handler handler : f_handlers) {
+			logger.addHandler(handler);
 		}
 	}
 
-	public static void addHandler(final Handler handler) {
-		handler.setFormatter(f_formatter);
+	/**
+	 * Adds a handler to the set of handlers used by SureLogic logging.
+	 * 
+	 * @param handler
+	 *            the handler to log.
+	 * 
+	 * @throws NullPointerException
+	 *             if the handler is null.
+	 */
+	public static synchronized void addHandler(final Handler handler) {
+		if (handler == null)
+			throw new NullPointerException("handler must be non-null");
 
+		handler.setFormatter(f_formatter.get());
+		f_handlers.add(handler);
+
+		/*
+		 * Add this handler to all the existing loggers.
+		 */
+		for (Logger logger : f_nameToLogger.values()) {
+			logger.addHandler(handler);
+		}
+	}
+
+	static {
+		addHandler(new ConsoleHandler());
+		try {
+			addHandler(new FileHandler(System.getProperty("java.io.tmpdir")
+					+ File.separator + "SureLogic_Log.txt", true));
+		} catch (Exception e) {
+			throw new IllegalStateException(
+					"Unable to create FileHandler object for SureLogic logger",
+					e);
+		}
 	}
 
 	/**
@@ -54,103 +98,51 @@ public class SLLogger extends Formatter {
 	 * Otherwise a new logger is created.
 	 * <p>
 	 * If a new logger is created its log level will be configured to use a
-	 * concise output format to the console (defined by this class) and it will
-	 * <i>not</i> send logging output to its parent's handlers. It will be
-	 * registered in the LogManager global name space.
-	 * <P>
-	 * if the system property <tt>no-fluid-logger</tt> was set as a virtual
-	 * machine argument then this method is equivalent to invoking:
-	 * 
-	 * <pre>
-	 * java.util.logging.Logger.getLogger(name);
-	 * </pre>
+	 * concise output format to the console (defined by {@link SLFormatter}) as
+	 * well as to the file <code>SureLogic_Log.txt</code> in the directory
+	 * specified by the <code>java.io.tmpdir</code> property. It will <i>not</i>
+	 * send logging output to its parent's handlers. It will be registered in
+	 * the LogManager global name space.
 	 * 
 	 * @param name
-	 *            name for the logger. This should be a dot-separated name and
-	 *            should normally be based on the package name or class name of
-	 *            the subsystem, such as java.net or javax.swing
-	 * @return a suitable Logger
+	 *            name for the logger. All names passed to this method become
+	 *            prefixed by <code>com.surelogic</code>.
+	 * @return a suitable Logger.
 	 * @throws NullPointerException
-	 *             if the name is null
+	 *             if the name is null.
 	 */
-	public static Logger getLogger(final String name) {
+	public static synchronized Logger getLogger(final String name) {
 		if (name == null)
 			throw new NullPointerException("name must be non-null");
+		final String loggerName = "com.surelogic"
+				+ ("".equals(name) ? "" : "." + name);
 
-		if (f_useSLLogger) {
-			synchronized (f_nameToLogger) {
-				Logger resultLogger = f_nameToLogger.get(name);
-				if (resultLogger == null) {
-					/*
-					 * Setup this logger for use.
-					 */
-					resultLogger = Logger.getLogger(name);
-					resultLogger.setUseParentHandlers(false);
+		Logger logger = f_nameToLogger.get(loggerName);
+		if (logger == null) {
+			logger = Logger.getLogger(loggerName);
+			f_nameToLogger.put(loggerName, logger); // add to cache
 
-					Handler consoleHandler = new ConsoleHandler();
-					consoleHandler.setFormatter(f_formatter);
-					resultLogger.addHandler(consoleHandler);
-
-					// Output the log to a file in temp directory
-					try {
-						String tempDir = System.getProperty("java.io.tmpdir");
-						Handler fileHandler = new FileHandler(tempDir
-								+ File.separator + "sierraLog.txt", true);
-						fileHandler.setFormatter(f_formatter);
-						resultLogger.addHandler(fileHandler);
-
-					} catch (SecurityException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-
-					f_nameToLogger.put(name, resultLogger); // add to cache
-				}
-				return resultLogger;
-			}
-		} else {
-			return Logger.getLogger(name);
+			/*
+			 * Setup this logger for use.
+			 */
+			logger.setUseParentHandlers(false);
+			addAllHandlersTo(logger);
 		}
+		return logger;
 	}
 
 	/**
-	 * Format the given log record and return the formatted string. The
-	 * resulting formatted String for a ConciseFormatter outputs single lines
-	 * using the following pattern:
-	 * <p>[ <i>LEVEL </i>" <i>thread name </i>"] <i>message </i>[
-	 * <i>package.class.method() </i>]
-	 * <p>
-	 * The {@link #formatMessage(LogRecord)} convenience method is used to
-	 * localize and format the message field.
+	 * Convenience method to get the <code>com.surelogic</code> logger.
+	 * Invoking this method is equivalent to invoking:
 	 * 
-	 * @param record
-	 *            the log record to be formatted.
-	 * @return the formatted log record.
+	 * <pre>
+	 * getLogger(&quot;&quot;)
+	 * </pre>
+	 * 
+	 * @return a suitable Logger.
+	 * 
 	 */
-	@Override
-	public String format(final LogRecord record) {
-		StringBuilder buf = new StringBuilder(1000);
-		buf.append("[");
-		buf.append(record.getLevel().getName());
-		buf.append(" \"");
-		buf.append(Thread.currentThread().getName());
-		buf.append("\" ");
-		buf.append(Calendar.getInstance().getTime().toString());
-		buf.append("]");
-		buf.append(formatMessage(record));
-		buf.append(" [");
-		buf.append(record.getSourceClassName());
-		buf.append(".");
-		buf.append(record.getSourceMethodName());
-		buf.append("()]");
-		buf.append('\n');
-		Throwable t = record.getThrown();
-		if (t != null) {
-			StringWriter sw = new StringWriter();
-			t.printStackTrace(new PrintWriter(sw));
-			buf.append(sw.toString());
-		}
-		return buf.toString();
+	public static synchronized Logger getLogger() {
+		return getLogger("");
 	}
 }
