c3a0ff252fd91e08dee102096e51041f65675e65
Tanmay Sinha <tanmay.sinha@surelogic.com>
2007-09-10 20:21:26 +0000

    Export wizard for sierra.xml
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@1473 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/logging/LockFileUtility.java
@@ -0,0 +1,57 @@
+package com.surelogic.common.logging;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * The utility for creating a lock file for a particular project
+ * 
+ * Current not in use
+ * 
+ * @author Tanmay.Sinha
+ * 
+ */
+@Deprecated
+public class LockFileUtility {
+
+	private static final LockFileUtility INSTANCE = new LockFileUtility();
+
+	public static LockFileUtility getInstance() {
+		return INSTANCE;
+	}
+
+	/**
+	 * returns true if it created a new lock file else returns false
+	 * 
+	 * @param productName
+	 * @param projectName
+	 * @param folderPath
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean obtainLockFile(String productName, String projectName,
+			String folderPath) throws IOException {
+
+		File lockFile = new File(folderPath + File.separator + productName
+				+ "-" + projectName + ".lock");
+
+		if (lockFile.createNewFile()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public boolean clearLockFile(String productName, String projectName,
+			String folderPath) {
+		File lockFile = new File(folderPath + File.separator + productName
+				+ "-" + projectName + ".lock");
+
+		if (lockFile.delete()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
