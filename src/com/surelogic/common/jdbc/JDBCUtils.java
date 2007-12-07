bbef7dc545db49cc1f56ee1c43daa07369a295fd
Edwin Chan <edwin.chan@surelogic.com>
2007-12-07 15:54:55 +0000

    Copied some code from sierra-jdbc
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@3765 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/jdbc/JDBCUtils.java
@@ -0,0 +1,142 @@
+package com.surelogic.common.jdbc;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.Date;
+
+/**
+ * A collection of utility methods to help w/ using JDBC.
+ * (a subset copied from Sierra)
+ * 
+ * @author nathan
+ */
+public class JDBCUtils {
+	
+	/**
+	 * Set a paramter to the specified String, or to null if none is supplied.
+	 * 
+	 * @param idx
+	 * @param st
+	 * @param string
+	 * @throws SQLException
+	 */
+	public static void setNullableString(int idx, PreparedStatement st,
+			String string) throws SQLException {
+		if (string == null) {
+			st.setNull(idx, Types.VARCHAR);
+		} else {
+			st.setString(idx, string);
+		}
+	}
+
+	/**
+	 * Set a paramter to the specified Long, or to null if none is supplied.
+	 * 
+	 * @param idx
+	 * @param st
+	 * @param longValue
+	 * @throws SQLException
+	 */
+	public static void setNullableLong(int idx, PreparedStatement st,
+			Long longValue) throws SQLException {
+		if (longValue == null) {
+			st.setNull(idx, Types.BIGINT);
+		} else {
+			st.setLong(idx, longValue);
+		}
+	}
+
+	/**
+	 * Set a paramter to the specified Integer, or to null if none is supplied.
+	 * 
+	 * @param idx
+	 * @param st
+	 * @param intValue
+	 * @throws SQLException
+	 */
+	public static void setNullableInt(int idx, PreparedStatement st,
+			Integer intValue) throws SQLException {
+		if (intValue == null) {
+			st.setNull(idx, Types.INTEGER);
+		} else {
+			st.setLong(idx, intValue);
+		}
+	}
+
+	/**
+	 * Set a parameter to the specified Date, or to null if none is supplied.
+	 * 
+	 * @param idx
+	 * @param st
+	 * @param dateValue
+	 * @throws SQLException
+	 */
+	public static void setNullableTimestamp(int idx, PreparedStatement st,
+			Date dateValue) throws SQLException {
+		if (dateValue == null) {
+			st.setNull(idx, Types.TIMESTAMP);
+		} else {
+			st.setTimestamp(idx, new Timestamp(dateValue.getTime()));
+		}
+	}
+
+	/**
+	 * Returns a Long, or null if the corresponding column was null in the
+	 * result set.
+	 * 
+	 * @param idx
+	 * @param set
+	 * @return
+	 * @throws SQLException
+	 */
+	public static Long getNullableLong(int idx, ResultSet set)
+			throws SQLException {
+		long l = set.getLong(idx);
+		if (set.wasNull()) {
+			return null;
+		} else {
+			return l;
+		}
+	}
+
+	/**
+	 * Returns an Integer, or null if the corresponding column was null in the
+	 * database.
+	 * 
+	 * @param idx
+	 * @param set
+	 * @return
+	 * @throws SQLException
+	 */
+	public static Integer getNullableInteger(int idx, ResultSet set)
+			throws SQLException {
+		int i = set.getInt(idx);
+		if (set.wasNull()) {
+			return null;
+		} else {
+			return i;
+		}
+	}
+
+	/**
+	 * The current system time as a timestamp.
+	 * 
+	 * @return
+	 */
+	public static Timestamp now() {
+		return new Timestamp(System.currentTimeMillis());
+	}
+
+	/**
+	 * Escape a string to be used as input in a JDBC query.
+	 * 
+	 * @param string
+	 * @return
+	 */
+	public static String escapeString(String string) {
+		return string.replaceAll("'", "''");
+	}
+}
