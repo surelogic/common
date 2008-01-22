7df8b9b737e5c61263015d7f92d55b24b0507772
Edwin Chan <edwin.chan@surelogic.com>
2008-01-22 18:34:37 +0000

    Factored out common code (some to XMLUtil)
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@5167 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/xml/XMLUtil.java
+++ src/com/surelogic/common/xml/XMLUtil.java
@@ -1,10 +1,15 @@
 package com.surelogic.common.xml;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class XMLUtil {
   public static SAXParser createSAXParser() {
@@ -20,5 +25,17 @@ public class XMLUtil {
       throw new IllegalStateException("Could not create SAX parser.", e);
     }
     return sp;
+  }
+  
+  public static void parseResource(Logger log, SAXParser sp, String resource,
+                                   DefaultHandler handler, String errMsg) {
+    try {
+      sp.parse(Thread.currentThread().getContextClassLoader()
+          .getResourceAsStream(resource), handler);
+    } catch (SAXException e) {
+      log.log(Level.WARNING, errMsg, e);
+    } catch (IOException e) {
+      log.log(Level.WARNING, errMsg, e);
+    }
   }
 }
