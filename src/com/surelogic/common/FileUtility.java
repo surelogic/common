172371ecf16ebfce57cebb1bcfce36f045b47f4a
Tim Halloran <tim.halloran@surelogic.com>
2008-01-31 15:33:55 +0000

    Changed Sierra data directory location
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@5509 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/FileUtility.java
+++ src/com/surelogic/common/FileUtility.java
@@ -11,10 +11,43 @@ public final class FileUtility {
 		// no instances
 	}
 
+	/**
+	 * Tries to create the specified directory in the file system unless it
+	 * already exists.
+	 * 
+	 * @param path
+	 *            the desired directory.
+	 * @returns <tt>true</tt> if the directory exited or was created, along
+	 *          with all necessary parent directories; <tt>false</tt>
+	 *          otherwise.
+	 * 
+	 */
+	static public boolean createDirectory(final String path) {
+		File p = new File(path);
+		if (p.exists()) {
+			return true;
+		} else {
+			boolean success = p.mkdirs();
+			if (!success) {
+				SLLogger.getLogger().warning(I18N.err(30, path));
+			}
+			return success;
+		}
+	}
+
+	/**
+	 * Tries to delete a directory and all its contents.
+	 * 
+	 * @param path
+	 *            the directory to delete
+	 * @return <tt>true</tt> if and only if the directory is successfully
+	 *         deleted; <tt>false</tt> otherwise.
+	 * 
+	 */
 	static public boolean deleteDirectoryAndContents(final File path) {
+		boolean success;
 		if (path.exists()) {
 			for (File file : path.listFiles()) {
-				boolean success;
 				if (file.isDirectory()) {
 					success = deleteDirectoryAndContents(file);
 				} else {
@@ -26,6 +59,39 @@ public final class FileUtility {
 				}
 			}
 		}
-		return (path.delete());
+		success = path.delete();
+		if (!success) {
+			SLLogger.getLogger().warning(I18N.err(11, path.getAbsolutePath()));
+		}
+		return success;
+	}
+
+	/**
+	 * This method gets the path to the Sierra data directory. It ensures the
+	 * directory exists.
+	 * <p>
+	 * It first tries to use the value of the <tt>SLSierraDataDirectory</tt>
+	 * property. If that property is not defined (or it is not valid) then
+	 * <tt>~/.sierra-data</tt> is used. Finally, if that directory is invalid
+	 * then the value of <tt>java.io.tmpdir</tt> is used.
+	 * 
+	 * @return the path to the Sierra data directory. No training <tt>/</tt>
+	 *         is included.
+	 */
+	static public String getSierraDataDirectory() {
+		String dir = System.getProperty("SLSierraDataDirectory");
+		/*
+		 * The property was set so see if it makes sense.
+		 */
+		if (dir != null) {
+			if (createDirectory(dir))
+				return dir;
+			SLLogger.getLogger().warning(I18N.err(31, dir));
+		}
+		dir = System.getProperty("user.home") + File.separator + ".sierra-data";
+		if (createDirectory(dir))
+			return dir;
+		SLLogger.getLogger().severe(I18N.err(32, dir));
+		return System.getProperty("java.io.tmpdir");
 	}
 }
