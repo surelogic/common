fecddb448447f8af8a6dd383806ad704541db955
Tim Halloran <tim.halloran@surelogic.com>
2008-02-08 19:35:52 +0000

    Cleaning up images
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@5788 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/update/FeatureVersions.java
+++ src/com/surelogic/common/update/FeatureVersions.java
@@ -5,39 +5,51 @@ import java.net.*;
 import java.util.Properties;
 
 public final class FeatureVersions extends Properties implements IFeatureVersionMap {
-  private static final long serialVersionUID = 1L;
-  
-  private static String LATEST = "http://www.surelogic.com/latest.properties";
-  
-  private FeatureVersions(String location) throws IOException {
-    URL site      = new URL(location);
-    load(site.openStream());
-  }
-  
-  public static IFeatureVersionMap getLatestVersions() throws IOException {
-    return new FeatureVersions(LATEST);
-  }
+	private static final long serialVersionUID = 1L;
 
-  public String get(String id) {
-    return (String) get((Object) id);
-  }
-  
-  public static void main(String[] args) {
-    try {
-      IFeatureVersionMap versions = new FeatureVersions("file:///work/workspace/common/latest.properties");
-      System.out.println("common = "+versions.get("common"));
+	private static final String LATEST = "http://www.surelogic.com/product-releases.properties";
 
-      String[] tmp = new String[] { "0.7", "1.0.0", "1.0.0.1", "2.0", "2.0_2008" };
-      String last  = null; 
-      for(String v : tmp) {
-        if (last != null) {
-          System.out.println(last+" <= "+v+" : "+(last.compareTo(v) <= 0));
-        }
-        last = v;
-      }
-      
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+	private FeatureVersions(String location) throws IOException {
+		URL site = new URL(location);
+		load(site.openStream());
+	}
+
+	public static IFeatureVersionMap getLatestVersions() throws IOException {
+		return new FeatureVersions(LATEST);
+	}
+
+	public String get(String productName) {
+		return (String) get((Object) productName);
+	}
+
+	public boolean upgradeAvailable(String productName, String currentVersion) {
+		final String latestVersion = get(productName);
+		if (latestVersion != null) {
+
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		try {
+			IFeatureVersionMap versions = new FeatureVersions(
+					"file:///Users/Tim/Documents/Source/SierraEclipseClient/common/latest.properties");
+			System.out.println("common = " + versions.get("common"));
+
+			String[] tmp = new String[] { "0.9", "1.0.0", "1.0.0.1", "2.0",
+					"2.0_2008" };
+			String last = null;
+			for (String v : tmp) {
+				if (last != null) {
+					System.out.println(last + " <= " + v + " : "
+							+ (last.compareTo(v) <= 0));
+				}
+				last = v;
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- src/com/surelogic/common/update/IFeatureVersionMap.java
+++ src/com/surelogic/common/update/IFeatureVersionMap.java
@@ -1,5 +1,29 @@
 package com.surelogic.common.update;
 
+/**
+ * Provides access to released versions of SureLogic software products.
+ */
 public interface IFeatureVersionMap {
-  String get(String id);
+	/**
+	 * Gets the version number for the newest release of a product from
+	 * SureLogic.
+	 * 
+	 * @param productName
+	 *            the product identifier.
+	 * @return the version number of the newest release from SureLogic, or
+	 *         <tt>null</tt> if the product identifier is unknown.
+	 */
+	String get(String productName);
+
+	/**
+	 * Checks if an upgrade is available for a given version of a product.
+	 * 
+	 * @param productName
+	 *            the product identifier.
+	 * @param currentVersion
+	 *            the installed version number of the product.
+	 * @return <tt>true</tt> if an upgrade is available for this product,
+	 *         <tt>false</tt> otherwise.
+	 */
+	boolean upgradeAvailable(String productName, String currentVersion);
 }
