150ecf9e49ca9e4d4a390b5b46afacf3d87273f0
Tim Halloran <tim.halloran@surelogic.com>
2008-03-04 22:41:15 +0000

    Made constant for (default package)
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@6497 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/JavaConstants.java
@@ -0,0 +1,10 @@
+package com.surelogic.common;
+
+public final class JavaConstants {
+
+	public static final String DEFAULT_PACKAGE = "(default package)";
+
+	private JavaConstants() {
+		// no instances
+	}
+}
