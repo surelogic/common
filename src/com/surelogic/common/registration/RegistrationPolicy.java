db5c10a8f070c1d89217edff42f6b8fbd1719c0e
Edwin Chan <edwin.chan@surelogic.com>
2008-04-02 15:01:07 +0000

    Added infrastructure to support product registration
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@7127 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/registration/RegistrationPolicy.java
@@ -0,0 +1,11 @@
+/*
+ * Created on Apr 2, 2008
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package com.surelogic.common.registration;
+
+public enum RegistrationPolicy {
+  NOT_YET, NEVER, LATER, DONE
+}
