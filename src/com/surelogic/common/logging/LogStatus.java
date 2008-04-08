648f81e53270a5d708fddee92c94e965f3f506a3
Edwin Chan <edwin.chan@surelogic.com>
2008-04-08 13:57:41 +0000

    Fixed up Eclipse-independent code
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@7223 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/logging/LogStatus.java
@@ -0,0 +1,7 @@
+package com.surelogic.common.logging;
+
+public class LogStatus {
+	public static void createErrorStatus(int code, String msg) {
+		throw new UnsupportedOperationException("Not impl'd yet");
+	}
+}
