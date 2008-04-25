79d5fc573e4f8b12b2e53c9cfb32f0e943e9f11a
Edwin Chan <edwin.chan@surelogic.com>
2008-04-25 14:52:10 +0000

    Moved XML code into common Moved code to create source HTML to common-eclipse
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@7571 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/xml/XMLConstants.java
@@ -0,0 +1,9 @@
+package com.surelogic.common.xml;
+
+public final class XMLConstants {
+  public static final String AMPERSAND = "&amp;";
+  public static final String APOSTROPHE = "&apos;";
+  public static final String DOUBLE_QUOTE = "&quot;";
+  public static final String GREATER_THAN = "&gt;";
+  public static final String LESS_THAN = "&lt;";
+}
--- src/com/surelogic/common/xml/XMLUtil.java
+++ src/com/surelogic/common/xml/XMLUtil.java
@@ -1,5 +1,7 @@
 package com.surelogic.common.xml;
 
+import static com.surelogic.common.xml.XMLConstants.*;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
@@ -39,4 +41,89 @@ public class XMLUtil {
       log.log(Level.WARNING, errMsg, e);
     }
   }
+  
+  public static String openNode(String tag) {
+	  return "<"+tag+">";
+  }
+
+  public static String startNode(String tag) {
+	  return "<"+tag+" ";
+  }
+
+  public static String closeNode(String tag) {
+	  return "</"+tag+">";
+  }
+
+  /**
+   * Includes a new line
+   */
+  public static String oneAttrNode(String tag, String attr, String val) {
+	  return startNode(tag) + lastAttr(attr, val);
+  }
+
+  public static String oneAttrOpen(String tag, String attr, String val) {
+	  return startNode(tag) + setAttr(attr, val) + ">\n";
+  }
+
+  public static String setAttr(String attr, String val) {
+	  return attr+"=\""+val+"\"";
+  }
+
+  /**
+   * Includes a new line
+   */
+  public static String lastAttr(String attr, String val) {
+	  return attr+"=\""+val+"\"/>\n";
+  }
+
+  public static String escape(String s) {
+	  StringBuilder result = new StringBuilder(s.length() + 20);
+	  char c;
+	  for (int i = 0; i < s.length(); ++i) {
+		  switch (c = s.charAt(i)) {
+		  case '<':
+			  result.append(LESS_THAN);
+			  break;
+		  case '>':
+			  result.append(GREATER_THAN);
+			  break;
+		  case '"':
+			  result.append(DOUBLE_QUOTE);
+			  break;
+		  case '\'':
+			  result.append(APOSTROPHE);
+			  break;
+		  case '&':
+			  result.append(AMPERSAND);
+			  break;
+		  default:
+			  result.append(c);
+		  }
+	  }
+	  return result.toString();
+  }
+
+  public static String unescape(String str) {
+	  StringBuffer result = new StringBuffer(str.length());
+	  for(int i=0; i<str.length(); i++) {
+		  char c = str.charAt(i);
+		  if(c != '&') result.append(c);
+		  else {
+			  int entityEnd = str.indexOf(';', i);
+			  if(entityEnd == -1) {
+				  // (bad) unquoted &, just add it and continue
+				  result.append('&');
+				  continue;
+			  }
+			  String entity = str.substring(i, entityEnd + 1);
+			  if(entity.equals(LESS_THAN)) result.append('<');
+			  else if (entity.equals(GREATER_THAN)) result.append('>');
+			  else if (entity.equals(DOUBLE_QUOTE)) result.append('"');
+			  else if (entity.equals(APOSTROPHE)) result.append('\'');
+			  else if (entity.equals(AMPERSAND)) result.append('&');
+			  i = entityEnd;
+		  }
+	  }
+	  return result.toString();
+  }  
 }
