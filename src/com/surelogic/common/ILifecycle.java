ddf02a7d3aa3b543647875a55b91cd3ef437a21b
Tim Halloran <tim.halloran@surelogic.com>
2008-07-22 17:25:59 +0000

    Working on Flashlight: Jobs moved into flashlight-common
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@8840 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/ILifecycle.java
+++ src/com/surelogic/common/ILifecycle.java
@@ -1,8 +1,18 @@
 package com.surelogic.common;
 
+/**
+ * Interface for objects that follow an initialization after construction, but
+ * before use, and disposal after use.
+ */
 public interface ILifecycle {
 
+	/**
+	 * Actions required after construction, but before use.
+	 */
 	void init();
 
+	/**
+	 * Actions required after use.
+	 */
 	void dispose();
 }
