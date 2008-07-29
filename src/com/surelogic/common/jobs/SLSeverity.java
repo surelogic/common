efe5038c14d644af0fa1fc0291a77298be63153b
Tim Halloran <tim.halloran@surelogic.com>
2008-07-29 17:12:51 +0000

    Added converter and Javadoc
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@9047 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/jobs/SLSeverity.java
+++ src/com/surelogic/common/jobs/SLSeverity.java
@@ -1,5 +1,50 @@
 package com.surelogic.common.jobs;
 
+import java.util.logging.Level;
+
+/**
+ * An IDE independent severity. This enumeration is used for IDE independent
+ * status reporting.
+ * 
+ * @see SLStatus
+ */
 public enum SLSeverity {
-	OK, INFO, CANCEL, WARNING, ERROR;
+	OK {
+		@Override
+		public Level toLevel() {
+			return Level.FINEST;
+		}
+	},
+	INFO {
+		@Override
+		public Level toLevel() {
+			return Level.INFO;
+		}
+	},
+	CANCEL {
+		@Override
+		public Level toLevel() {
+			return Level.FINEST;
+		}
+	},
+	WARNING {
+		@Override
+		public Level toLevel() {
+			return Level.WARNING;
+		}
+	},
+	ERROR {
+		@Override
+		public Level toLevel() {
+			return Level.SEVERE;
+		}
+	};
+
+	/**
+	 * Converts this severity to a {@link Level} usable by the logging
+	 * infrastructure.
+	 * 
+	 * @return a logging level.
+	 */
+	public abstract Level toLevel();
 }
