98b5d2026bb4b8d04e466606fc51b8ad962f8ee6
Tim Halloran <tim.halloran@surelogic.com>
2008-08-18 21:15:43 +0000

    Working on and IDE independent ad hoc query model.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@9343 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/SLUtility.java
+++ src/com/surelogic/common/SLUtility.java
@@ -1,5 +1,9 @@
 package com.surelogic.common;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * A utility with SureLogic common code.
  */
@@ -19,6 +23,33 @@ public final class SLUtility {
 	public static int safeLongToInt(long value) {
 		long result = Math.min(value, Integer.MAX_VALUE);
 		return (int) result;
+	}
+
+	private final static ThreadLocal<SimpleDateFormat> tl_format = new ThreadLocal<SimpleDateFormat>() {
+		@Override
+		protected SimpleDateFormat initialValue() {
+			return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		}
+	};
+
+	public static synchronized String toStringMS(final Date date) {
+		return tl_format.get().format(date);
+	}
+
+	public static Timestamp getWall(final Timestamp start, final long startNS,
+			final long timeNS) {
+		long tMS = start.getTime();
+		final long deltaNS = timeNS - startNS;
+		if (deltaNS < 0)
+			throw new IllegalStateException("timeNS=" + timeNS
+					+ " cannot be less than startedNS=" + startNS);
+		final long deltaMS = deltaNS / 1000000;
+		tMS = tMS + deltaMS;
+		long tDecNS = (tMS % 1000) * 1000000;
+		tDecNS = tDecNS + (deltaNS % 1000000);
+		Timestamp result = new Timestamp(tMS);
+		result.setNanos((int) tDecNS);
+		return result;
 	}
 
 	private SLUtility() {
