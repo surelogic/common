d8bd5389ca9e39b0784ad141453b15db9456ed11
Aaron Greenhouse <aaron.greenhouse@surelogic.com>
2008-09-30 14:51:26 +0000

    Updated to use a java.util.concurrent.Executor
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@9809 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/jobs/ConsoleJob.java
+++ src/com/surelogic/common/jobs/ConsoleJob.java
@@ -1,5 +1,11 @@
 package com.surelogic.common.jobs;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 /**
  * Class for submitting jobs to a "console." The jobs are not meant to be
  * interactive, and thus the console is really some king of output-only
@@ -11,19 +17,46 @@ package com.surelogic.common.jobs;
  * 
  * <p>Jobs are executed serially.
  */
-/* 
- * TODO: Use ExecuteServices and Executors.newSingleThreadExecutor() plus futures
- */
 public final class ConsoleJob {
   private final SLProgressMonitorFactory pmFactory;
+  private final ExecutorService executor = Executors.newSingleThreadExecutor();
+  
+  
+  
+  private static final class ConsoleCallable implements Callable<SLStatus> {
+    private final SLJob slJob;
+    private final SLProgressMonitor monitor;
+    
+    public ConsoleCallable(final SLJob slj, final SLProgressMonitor pm) {
+      slJob = slj;
+      monitor = pm;
+    }
+    
+    public SLStatus call()  {
+      return slJob.run(monitor);
+    }    
+  }
+  
+  
   
   public ConsoleJob(final SLProgressMonitorFactory pmf) {
     pmFactory = pmf;
   }
-  
+
+  /**
+   * Execute the given job.  Doesn't return until the job terminates.
+   * @param slJob The job to execute.
+   * @return The status of the job.
+   */
   public SLStatus submitJob(final SLJob slJob) {
     final SLProgressMonitor monitor = pmFactory.createSLProgressMonitor(slJob.getName());
-    final SLStatus status = slJob.run(monitor);
-    return status;
+    final Future<SLStatus> future = executor.submit(new ConsoleCallable(slJob, monitor));
+    try {
+      return future.get();
+    } catch (InterruptedException e) {
+      return SLStatus.createErrorStatus(e);
+    } catch (ExecutionException e) {
+      return SLStatus.createErrorStatus(e);
+    }
   }
 }
