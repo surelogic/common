47366aaf49be93c0699658f406d674902d139f2a
Tim Halloran <tim.halloran@surelogic.com>
2008-10-08 15:28:00 +0000

    Working on license management
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@9920 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/license/SLLicenseUtility.java
+++ src/com/surelogic/common/license/SLLicenseUtility.java
@@ -1,13 +1,102 @@
 package com.surelogic.common.license;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.security.auth.x500.X500Principal;
 
 import com.surelogic.common.i18n.I18N;
 
+import de.schlichtherle.license.LicenseContent;
+import de.schlichtherle.license.LicenseManager;
+
 public final class SLLicenseUtility {
 
-	public static final String ALL_TOOL_SUBJECT = "SureLogic All-Tool License";
-	public static final String FLASHLIGHT_SUBJECT = "SureLogic Flashlight License";
+	/*
+	 * Keep the SUBJECTS array complete and in alphabetical order when new
+	 * license subjects are added below.
+	 */
+	public static final String ALL_TOOL_SUBJECT = "All SureLogic Tools";
+	public static final String FLASHLIGHT_SUBJECT = "SureLogic Flashlight";
+	public static final String JSURE_SUBJECT = "SureLogic JSure";
+	public static final String SIERRA_SUBJECT = "SureLogic Sierra";
+
+	public static final String[] SUBJECTS = { ALL_TOOL_SUBJECT,
+			FLASHLIGHT_SUBJECT, JSURE_SUBJECT, SIERRA_SUBJECT };
+
+	/**
+	 * Gets a possibly empty list of the installed licenses.
+	 * 
+	 * @return a possibly empty list of the installed licenses.
+	 */
+	public static List<LicenseContent> getInstalledLicenses() {
+		final List<LicenseContent> result = new ArrayList<LicenseContent>();
+		/*
+		 * Collect up the possibly installed licenses.
+		 */
+		LicenseContent lc;
+		for (String subject : SUBJECTS) {
+			lc = tryToGetInstalledLicense(subject);
+			if (lc != null)
+				result.add(lc);
+		}
+		return result;
+	}
+
+	/**
+	 * Gets the content of an installed license with the passed subject, or
+	 * {@code null} if no such license is installed.
+	 * 
+	 * @param subject
+	 *            the non-null license subject.
+	 * @return the content of an installed license with the passed subject, or
+	 *         {@code null} if no such license is installed.
+	 */
+	public static LicenseContent tryToGetInstalledLicense(final String subject) {
+		if (subject == null)
+			throw new IllegalArgumentException(I18N.err(44, "subject"));
+		final LicenseManager lm = new LicenseManager(
+				new SLLicenseParam(subject));
+		LicenseContent lc = null;
+		try {
+			lc = lm.verify();
+		} catch (Exception e) {
+			// ignore, the license is not installed, return null.
+		}
+		return lc;
+	}
+
+	/**
+	 * Tries to install a license for the passed subject from the passed file.
+	 * This method will throw an exception if anything goes wrong during the
+	 * installation.
+	 * 
+	 * @param subject
+	 *            the non-null license subject.
+	 * @param keyFile
+	 *            the non-null file file to load the license key from. This file
+	 *            must exist and be able to be read from.
+	 * @throws Exception
+	 *             if the installation of the license fails for various reasons.
+	 *             Note that you should always use
+	 *             {@link Throwable#getLocalizedMessage()} to get a (possibly
+	 *             localized) meaningful message.
+	 */
+	public static void tryToInstallLicense(final String subject,
+			final File keyFile) throws Exception {
+		if (subject == null)
+			throw new IllegalArgumentException(I18N.err(44, "subject"));
+		if (keyFile == null)
+			throw new IllegalArgumentException(I18N.err(44, "keyFile"));
+		if (!keyFile.canRead())
+			throw new IllegalStateException(I18N.err(140, keyFile
+					.getAbsolutePath()));
+
+		final LicenseManager lm = new LicenseManager(
+				new SLLicenseParam(subject));
+		lm.install(keyFile);
+	}
 
 	/**
 	 * Creates an X500 principal for SureLogic.
