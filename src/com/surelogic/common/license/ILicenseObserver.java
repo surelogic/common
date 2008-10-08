956228ec57d30c71ba2d6a6705829b49fb4ab171
Tim Halloran <tim.halloran@surelogic.com>
2008-10-08 20:17:34 +0000

    Working on license management.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@9942 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/license/ILicenseObserver.java
+++ src/com/surelogic/common/license/ILicenseObserver.java
@@ -6,12 +6,13 @@ package com.surelogic.common.license;
 public interface ILicenseObserver {
 
 	/**
-	 * Indicates that a check was requested for the passed license subject, but
-	 * no such license is installed.
+	 * Indicates that a license check for a license subject failed. No no such
+	 * license is installed, nor is there an all SureLogic tools license
+	 * installed.
 	 * 
 	 * @param subject
 	 *            the non-null license subject.
+	 * @see SLLicenseUtility#validate(String)
 	 */
 	void notifyNoLicenseFor(String subject);
-
 }
