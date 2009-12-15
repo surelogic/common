8762570fbe483ccb7224049be0cc5055e1d6ba48
Edwin Chan <edwin.chan@surelogic.com>
2009-12-15 21:44:22 +0000

    Refactored to expose title prefix Added code to handle Bugzilla
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@13738 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/serviceability/ServiceabilityConstants.java
+++ src/com/surelogic/common/serviceability/ServiceabilityConstants.java
@@ -1,6 +1,7 @@
 package com.surelogic.common.serviceability;
 
 public interface ServiceabilityConstants {
-	String USAGE_LOG_TITLE = "--- Contents of the SureLogic program feature usage log (from ~/.surelogic) ---";
-	String ECLIPSE_LOG_TITLE = "--- Contents of the Eclipse log file (from ./.metadata/.log) ---";
+	String TITLE_PREFIX      = "--- Contents of the ";
+	String USAGE_LOG_TITLE   = TITLE_PREFIX+"SureLogic program feature usage log (from ~/.surelogic) ---";
+	String ECLIPSE_LOG_TITLE = TITLE_PREFIX+"Eclipse log file (from ./.metadata/.log) ---";
 }
