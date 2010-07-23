98dc843dbdf06e1a4331ce576b025bdc473cba13
Tim Halloran <tim.halloran@surelogic.com>
2010-07-23 21:41:17 +0000

    Working on http://surelogic.com/bugzilla/show_bug.cgi?id=47
    Run a default query via a double-click on a result row
    
    Should be working now.
    
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@15288 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/CommonImages.java
+++ src/com/surelogic/common/CommonImages.java
@@ -114,6 +114,7 @@ public final class CommonImages {
 	public static final String IMG_PROMISE_INCONSISTENT = "promise_inconsistent.gif";
 	public static final String IMG_PROMISE = "promise.gif";
 	public static final String IMG_QUERY_BACK = "query_back.gif";
+	public static final String IMG_QUERY_DEFAULT = "query_default.gif";
 	public static final String IMG_QUERY_EMPTY = "query_empty.gif";
 	public static final String IMG_QUERY_GRAY = "query_gray.gif";
 	public static final String IMG_QUERY_OBJ_HIDDEN = "query_obj_hidden.gif";
