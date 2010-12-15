ff9ce2dccb3ea355706debe6271d674a20cba899
Nathan Boy <nathan.boy@surelogic.com>
2010-12-15 20:11:44 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@16062 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/jdbc/LimitRowHandler.java
@@ -0,0 +1,52 @@
+package com.surelogic.common.jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A {@link RowHandler} that will only pull a certain number of rows of data. It
+ * may produce fewer if the number of rows available is less than the limit.
+ * 
+ * @author nathan
+ * 
+ * @param <T>
+ */
+public final class LimitRowHandler<T> implements ResultHandler<List<T>> {
+
+	private final RowHandler<T> handler;
+	private final int limit;
+
+	private LimitRowHandler(final RowHandler<T> handler, final int limit) {
+		this.handler = handler;
+		this.limit = limit;
+	}
+
+	public List<T> handle(final Result result) {
+		int count = 0;
+		List<T> resultList = new ArrayList<T>();
+		for (Row r : result) {
+			if (count < limit) {
+				resultList.add(handler.handle(r));
+			} else {
+				return resultList;
+			}
+		}
+		return resultList;
+	}
+
+	/**
+	 * Constructs a handler that will handle at most the given number of rows.
+	 * 
+	 * @param <T>
+	 * @param handler
+	 *            the handler used to produce values
+	 * @param limit
+	 *            the maximum number of values to produce
+	 * @return
+	 */
+	public static <T> ResultHandler<List<T>> from(final RowHandler<T> handler,
+			final int limit) {
+		return new LimitRowHandler<T>(handler, limit);
+	}
+
+}
