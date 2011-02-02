28ca6163f67e81d56a0fa1461a35946b64ed7fa0
Tim Halloran <tim.halloran@surelogic.com>
2011-02-02 23:45:25 +0000

    Working on core/UI separation of Eclipse dependencies
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common-core-eclipse@16581 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/core/JavaProjectResources.java
+++ src/com/surelogic/common/core/JavaProjectResources.java
@@ -6,6 +6,7 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.jdt.core.*;
 
 public class JavaProjectResources {
+	//TODO why are all these public -- seems like a bad API
 	public final IJavaProject project;
 	public final List<IResource> resources = new ArrayList<IResource>();
     public final List<ICompilationUnit> cus = new ArrayList<ICompilationUnit>();
