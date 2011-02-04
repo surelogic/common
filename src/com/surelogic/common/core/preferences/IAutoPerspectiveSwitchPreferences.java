416b4ccadaf0e86312755a619113eba2dce4fa32
Tim Halloran <tim.halloran@surelogic.com>
2011-02-04 17:41:13 +0000

    Fixing preferences.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common-core-eclipse@16652 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/core/preferences/IAutoPerspectiveSwitchPreferences.java
+++ src/com/surelogic/common/core/preferences/IAutoPerspectiveSwitchPreferences.java
@@ -1,22 +1,54 @@
 package com.surelogic.common.core.preferences;
 
+import com.surelogic.common.core.EclipseUtility;
+
 /**
- * For plug ins that provide similar functionality with regard to automatically
- * switching to their perspective but prompting the user if this is okay, but
- * use separate preference settings.
+ * For plug-ins that provide similar functionality with regard to automatically
+ * switching to their perspective but prompting the user (with a dialog) if this
+ * perspective switch is okay, but use separate preference settings.
  */
-public interface IAutoPerspectiveSwitchPreferences {
-	String getPrefConstant(String suffix);
+public abstract class IAutoPerspectiveSwitchPreferences {
+	/**
+	 * Boolean preference suffix that indicates if the user should be prompted
+	 * by a dialog to switch to another perspective.
+	 */
+	private static final String PROMPT_PERSPECTIVE_SWITCH = "perspective.switch.prompt";
 
-	String PROMPT_PERSPECTIVE_SWITCH = "perspective.switch.prompt";
+	public final String getPromptPerspectiveSwitchConstant() {
+		return getConstant(PROMPT_PERSPECTIVE_SWITCH);
+	}
 
-	boolean getPromptForPerspectiveSwitch();
+	public final boolean getPromptPerspectiveSwitch() {
+		return EclipseUtility
+				.getBooleanPreference(getPromptPerspectiveSwitchConstant());
+	}
 
-	void setPromptForPerspectiveSwitch(boolean value);
+	/**
+	 * boolean preference suffix that indicates that the perspective switch
+	 * should be performed automatically.
+	 */
+	private static final String AUTO_PERSPECTIVE_SWITCH = "perspective.switch.auto";
 
-	String AUTO_PERSPECTIVE_SWITCH = "perspective.switch.auto";
+	public final String getAutoPerspectiveSwitchConstant() {
+		return getConstant(AUTO_PERSPECTIVE_SWITCH);
+	}
 
-	boolean getAutoPerspectiveSwitch();
+	public final boolean getAutoPerspectiveSwitch() {
+		return EclipseUtility
+				.getBooleanPreference(getAutoPerspectiveSwitchConstant());
+	}
 
-	void setAutoPerspectiveSwitch(boolean value);
+	/**
+	 * Transforms a key suffix (typically {@link #PROMPT_PERSPECTIVE_SWITCH} or
+	 * {@link #AUTO_PERSPECTIVE_SWITCH}) by adding the correct prefix value and
+	 * returning a complete preference key.
+	 * <p>
+	 * The returned preference key can be used in the SureLogic preference API
+	 * (see {@link EclipseUtility#PREFERENCES_NODE})
+	 * 
+	 * @param suffix
+	 *            the suffix of a preference key.
+	 * @return a complete preference key.
+	 */
+	protected abstract String getConstant(String suffix);
 }
