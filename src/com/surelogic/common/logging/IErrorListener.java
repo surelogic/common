bd4f2b6dca36d0cf0fc20f82273b87fdde7f6635
Edwin Chan <edwin.chan@surelogic.com>
2011-02-17 15:13:23 +0000

    Moved PersistentDropInfo to jsure-core-eclipse Updated ScriptReader to match changes to the JSure model
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@16957 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/logging/IErrorListener.java
+++ src/com/surelogic/common/logging/IErrorListener.java
@@ -2,4 +2,11 @@ package com.surelogic.common.logging;
 
 public interface IErrorListener {
 	void reportError(String summary, String msg);
+	
+	IErrorListener throwListener = new IErrorListener() {
+		@Override
+		public void reportError(String summary, String msg) {
+			throw new IllegalStateException(msg);
+		}
+	};
 }
