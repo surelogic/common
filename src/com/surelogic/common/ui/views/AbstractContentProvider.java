fce6bf7250c3280c79a974b16b3a6f1765145864
Edwin Chan <edwin.chan@surelogic.com>
2011-06-16 20:51:26 +0000

    Abstract impl of a content/label provider
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common-ui-eclipse@17814 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/ui/views/AbstractContentProvider.java
@@ -0,0 +1,45 @@
+package com.surelogic.common.ui.views;
+
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.graphics.Image;
+
+public abstract class AbstractContentProvider 
+implements IStructuredContentProvider, ILabelProvider {
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Image getImage(Object element) {
+		return null;
+	}
+
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
