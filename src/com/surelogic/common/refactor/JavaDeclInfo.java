5a0f3115dfbf1fc9f7b7849d93af4e47728d7acc
Edwin Chan <edwin.chan@surelogic.com>
2011-08-10 12:16:15 +0000

    Fixed to properly detect whether the method is implicit
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@18229 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/refactor/JavaDeclInfo.java
+++ src/com/surelogic/common/refactor/JavaDeclInfo.java
@@ -58,7 +58,8 @@ public class JavaDeclInfo extends Entity implements IJavaDeclInfoClient {
 			return new Field((TypeContext) parent, attributes.get(Field.FIELD));
 		case METHOD:
 			String[] params = separateByColons(attributes.get(Method.PARAMS));
-			return new Method((TypeContext) parent, attributes.get(Method.METHOD), params, attributes.containsKey(Method.IS_IMPLICIT));
+			return new Method((TypeContext) parent, attributes.get(Method.METHOD), params, 
+					          "true".equals(attributes.get(Method.IS_IMPLICIT)));
 		case PARAM:
 			return new MethodParameter((Method) parent, 
 					Integer.valueOf(attributes.get(MethodParameter.PARAM_NUM))); 
