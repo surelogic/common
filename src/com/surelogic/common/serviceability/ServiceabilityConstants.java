e62be69dbb3fbd0534d4305e7d64bd6eb4bb278a
Tim Halloran <tim.halloran@surelogic.com>
2011-08-19 20:59:02 +0000

    removed "the" from the title.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@18393 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/serviceability/ServiceabilityConstants.java
+++ src/com/surelogic/common/serviceability/ServiceabilityConstants.java
@@ -1,5 +1,5 @@
 package com.surelogic.common.serviceability;
 
 public interface ServiceabilityConstants {
-	String TITLE_PREFIX = "--- Contents of the ";
+	String TITLE_PREFIX = "--- Contents of ";
 }
