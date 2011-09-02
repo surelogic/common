9ed76f5556b7e91f573939626ea6275ef6ced6ba
Tim Halloran <tim.halloran@surelogic.com>
2011-09-02 14:48:08 +0000

    Moved crash reporter into common (JSure and Sierra)
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common@18599 d02ae147-ee12-4a35-9452-91c89e8b2592

---
+++ src/com/surelogic/common/serviceability/scan/IScanCrashReporter.java
@@ -0,0 +1,27 @@
+package com.surelogic.common.serviceability.scan;
+
+import java.io.File;
+
+import com.surelogic.common.jobs.SLStatus;
+
+/**
+ * Interface for external scan crash reporters.
+ * <p>
+ * This abstraction is necessary because the Eclipse UI, which is used by the
+ * Eclipse JSure and Eclipse clients to report scan failures, is not available
+ * to the code that runs the external scans.
+ */
+public interface IScanCrashReporter {
+
+	/**
+	 * Reports a analysis scan crash. Can be invoked from any thread context.
+	 * 
+	 * @param status
+	 *            the status built up about the scan filter. Cannot be
+	 *            {@code null}.
+	 * @param scanLog
+	 *            the log file for the scan within the scan directory. Cannot be
+	 *            {@code null}, but reporters should check that the file exists.
+	 */
+	void reportJSureScanCrash(SLStatus status, File scanLog);
+}
