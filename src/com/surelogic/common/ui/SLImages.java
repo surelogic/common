d4bafd2a6fe637722fc023e8958327096ea5b1ec
Nathan Boy <nathan.boy@surelogic.com>
2011-10-05 21:00:49 +0000

    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common-ui-eclipse@18776 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/ui/SLImages.java
+++ src/com/surelogic/common/ui/SLImages.java
@@ -14,82 +14,85 @@ import com.surelogic.common.CommonImages;
  */
 public final class SLImages {
 
-	private SLImages() {
-		// no instances
-	}
+    private SLImages() {
+        // no instances
+    }
 
-	/**
-	 * Our map from keys to images so that we cache them (for sharing) and can
-	 * dispose of what we create.
-	 */
-	private static final Map<String, Image> f_keyToImage = new HashMap<String, Image>();
+    /**
+     * Our map from keys to images so that we cache them (for sharing) and can
+     * dispose of what we create.
+     */
+    private static final Map<String, Image> f_keyToImage = new HashMap<String, Image>();
 
-	/**
-	 * Returns the shared image managed under the given name.
-	 * <p>
-	 * Note that clients <b>must not</b> dispose the image returned by this
-	 * method.
-	 * 
-	 * @param symbolicName
-	 *            the symbolic name of the image; one of the
-	 *            <code>SharedImages.IMG_*</code> constants declared in
-	 *            {@link CommonImages}.
-	 * @return the image, or <code>null</code> if not found.
-	 */
-	public static synchronized Image getImage(String symbolicName) {
-		if (symbolicName == null)
-			return null;
-		Image result = f_keyToImage.get(symbolicName);
-		if (result != null) {
-			return result;
-		} else {
-			final ImageDescriptor imageDescriptor = getImageDescriptor(symbolicName);
-			if (imageDescriptor != null) {
-				result = imageDescriptor.createImage();
-				if (result != null) {
-					f_keyToImage.put(symbolicName, result);
-					return result;
-				}
-			}
-		}
-		return result;
-	}
+    /**
+     * Returns the shared image managed under the given name.
+     * <p>
+     * Note that clients <b>must not</b> dispose the image returned by this
+     * method.
+     * 
+     * @param symbolicName
+     *            the symbolic name of the image; one of the
+     *            <code>SharedImages.IMG_*</code> constants declared in
+     *            {@link CommonImages}.
+     * @return the image, or <code>null</code> if not found.
+     */
+    public static synchronized Image getImage(final String symbolicName) {
+        if (symbolicName == null) {
+            return null;
+        }
+        Image result = f_keyToImage.get(symbolicName);
+        if (result != null) {
+            return result;
+        } else {
+            final ImageDescriptor imageDescriptor = getImageDescriptor(symbolicName);
+            if (imageDescriptor != null) {
+                result = imageDescriptor.createImage();
+                if (result != null) {
+                    f_keyToImage.put(symbolicName, result);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
 
-	/**
-	 * Returns the image descriptor managed under the given name.
-	 * <p>
-	 * Unlike {@link Image}s, image descriptors themselves do not need to be
-	 * disposed.
-	 * 
-	 * @param symbolicName
-	 *            the symbolic name of the image; one of the
-	 *            <code>SharedImages.IMG_*</code> constants declared in this
-	 *            class.
-	 * @return the image descriptor, or <code>null</code> if not found.
-	 */
-	public static synchronized ImageDescriptor getImageDescriptor(
-			String symbolicName) {
-		if (symbolicName == null)
-			return null;
-		ImageDescriptor result = null;
-		URL url = CommonImages.getImageURL(symbolicName);
-		if (url != null) {
-			result = ImageDescriptor.createFromURL(url);
-			if (result != null)
-				return result;
-		}
-		return result;
-	}
+    /**
+     * Returns the image descriptor managed under the given name.
+     * <p>
+     * Unlike {@link Image}s, image descriptors themselves do not need to be
+     * disposed.
+     * 
+     * @param symbolicName
+     *            the symbolic name of the image; one of the
+     *            <code>SharedImages.IMG_*</code> constants declared in this
+     *            class.
+     * @return the image descriptor, or <code>null</code> if not found.
+     */
+    public static synchronized ImageDescriptor getImageDescriptor(
+            final String symbolicName) {
+        if (symbolicName == null) {
+            return null;
+        }
+        ImageDescriptor result = null;
+        URL url = CommonImages.getImageURL(symbolicName);
+        if (url != null) {
+            result = ImageDescriptor.createFromURL(url);
+            if (result != null) {
+                return result;
+            }
+        }
+        return result;
+    }
 
-	/**
-	 * <b>Not intended to be called by client code</b>.
-	 * <p>
-	 * Only invoked by {@link Activator} during a plug-in stop.
-	 */
-	static synchronized void dispose() {
-		for (Image image : f_keyToImage.values()) {
-			image.dispose();
-		}
-		f_keyToImage.clear();
-	}
+    /**
+     * <b>Not intended to be called by client code</b>.
+     * <p>
+     * Only invoked by {@link Activator} during a plug-in stop.
+     */
+    static synchronized void dispose() {
+        for (Image image : f_keyToImage.values()) {
+            image.dispose();
+        }
+        f_keyToImage.clear();
+    }
 }
