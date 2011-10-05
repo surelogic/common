ca491a0d1e48f7dc07cb2d3b93512c3abb7e9387
Tim Halloran <tim.halloran@surelogic.com>
2011-10-05 19:39:45 +0000

    Quick check on RfR internal use.
    git-svn-id: https://fluid.surelogic.com/svn/eng/trunk/common-ui-eclipse@18773 d02ae147-ee12-4a35-9452-91c89e8b2592

--- src/com/surelogic/common/ui/dialogs/ScopedResourcesSelectionDialog.java
+++ src/com/surelogic/common/ui/dialogs/ScopedResourcesSelectionDialog.java
@@ -3,7 +3,6 @@ package com.surelogic.common.ui.dialogs;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.debug.internal.ui.*;
-import org.eclipse.debug.internal.ui.launchConfigurations.*;
 import org.eclipse.jface.dialogs.*;
 import org.eclipse.jface.viewers.IBaseLabelProvider;
 import org.eclipse.jface.viewers.IContentProvider;
@@ -49,51 +48,30 @@ public class ScopedResourcesSelectionDialog extends AbstractDebugCheckboxSelecti
 		//setShowSelectAllButtons(true);
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.launchConfigurations.AbstractDebugSelectionDialog#getContentProvider()
-	 */
 	protected IContentProvider getContentProvider() {
 		return fContentProvider;
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.launchConfigurations.AbstractDebugSelectionDialog#getLabelProvider()
-	 */
 	protected IBaseLabelProvider getLabelProvider() {
 		return fLabelProvider;
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.launchConfigurations.AbstractDebugSelectionDialog#getDialogSettingsId()
-	 */
 	protected String getDialogSettingsId() {
 		return SETTINGS_ID;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.launchConfigurations.AbstractDebugSelectionDialog#getHelpContextId()
-	 */
 	protected String getHelpContextId() {
 		return IDebugHelpContextIds.SELECT_RESOURCES_TO_SAVE_DIALOG;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.launchConfigurations.AbstractDebugSelectionDialog#getViewerInput()
-	 */
 	protected Object getViewerInput() {
 		return fInput;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.launchConfigurations.AbstractDebugSelectionDialog#getViewerLabel()
-	 */
 	protected String getViewerLabel() {
 		return "Select resources to save:";
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.launchConfigurations.AbstractDebugCheckboxSelectionDialog#addCustomFooterControls(org.eclipse.swt.widgets.Composite)
-	 */
 	protected void addCustomFooterControls(Composite parent) {
 		super.addCustomFooterControls(parent);
 		fSavePref = new Button(parent, SWT.CHECK);
@@ -105,24 +83,15 @@ public class ScopedResourcesSelectionDialog extends AbstractDebugCheckboxSelecti
 		});
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.launchConfigurations.AbstractDebugCheckboxSelectionDialog#okPressed()
-	 */
 	protected void okPressed() {
 		fConfig.setAlwaysSavePref(fSavePref.getSelection());
 		super.okPressed();
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.launchConfigurations.AbstractDebugCheckboxSelectionDialog#addViewerListeners(org.eclipse.jface.viewers.StructuredViewer)
-	 */
 	protected void addViewerListeners(StructuredViewer viewer) {
 		// Override to remove listener that affects the ok button
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.internal.ui.AbstractDebugCheckboxSelectionDialog#isValid()
-	 */
 	protected boolean isValid() {
 		return true;
 	}
