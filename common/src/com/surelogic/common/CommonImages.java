698486921cf7212bbdccea8b2da7d23370a5523c
Tim Halloran <tim.halloran@surelogic.com>
2011-12-13 13:10:41 -0500

    Fixing images of annotations

--- common/src/com/surelogic/common/CommonImages.java
+++ common/src/com/surelogic/common/CommonImages.java
@@ -27,7 +27,7 @@ public final class CommonImages {
 	public static final String IMG_ANNOTATION_DECR = "annotation_decr.gif";
 	public static final String IMG_ANNOTATION_DELTA = "annotation_delta.gif";
 	public static final String IMG_ANNOTATION_ERROR = "annotation_error.gif";
-	public static final String IMG_ANNOTATION_ERROR_FIX = "annotation_error_fix.gif";
+	public static final String IMG_ANNOTATION_ERROR_PROPOSED = "annotation_error_proposed.gif";
 	public static final String IMG_ANNOTATION_PROPOSED = "annotation_proposed.gif";
 	public static final String IMG_ARROW_DOWN = "arrow_down.gif";
 	public static final String IMG_ARROW_RIGHT = "arrow_right.gif";
