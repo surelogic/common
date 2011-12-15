c310ea514026e98dd822f2574f062336a907ff4a
Edwin Chan <edwin.chan@surelogic.com>
2011-12-15 10:28:12 -0500

    Changed to extend Action

--- common-ui-eclipse/src/com/surelogic/common/ui/actions/AbstractMainAction.java
+++ common-ui-eclipse/src/com/surelogic/common/ui/actions/AbstractMainAction.java
@@ -1,11 +1,12 @@
 package com.surelogic.common.ui.actions;
 
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
-public abstract class AbstractMainAction implements IWorkbenchWindowActionDelegate {
+public abstract class AbstractMainAction extends Action implements IWorkbenchWindowActionDelegate {
 
     public void dispose() {
         // Nothing to do
