a51e1ca611b46b68591e606cf00f0151a05e05e6
Edwin Chan <edwin.chan@surelogic.com>
2012-04-06 10:21:00 -0400

    Added flag to force JSure to load all libraries

--- common/src/com/surelogic/common/XUtil.java
+++ common/src/com/surelogic/common/XUtil.java
@@ -14,7 +14,8 @@ package com.surelogic.common;
 public final class XUtil {
 	public static final String testingProp = "dc.testing";	
 	public static final String ignoreProposalsProp = "jsure.test.ignore.proposals";
-	
+	public static final String LOAD_ALL_LIBS = "SureLogic.loadAllLibs";
+		
 	private static final String f_updateScript = System.getProperty("SureLogicUpdateScript");
 	static {
 		if (f_updateScript != null) {
@@ -25,6 +26,8 @@ public final class XUtil {
 	public static final boolean testing = System.getProperty(testingProp,"false").equals("true");
 	public static final boolean testingWorkspace = System.getProperty("jsure.test.workspace","false").equals("true");
 	public static final boolean ignoreProposals = System.getProperty(ignoreProposalsProp,"false").equals("true");
+	
+	public static final boolean loadAllLibs = System.getProperty(LOAD_ALL_LIBS,"false").equals("true");
 	
 	/**
 	 * For doing remote debugging on the JSure JVM
