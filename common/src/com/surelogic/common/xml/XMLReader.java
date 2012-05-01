d90e3334709494b44b7068f5625f957163a6cf97
Tim Halloran <tim.halloran@surelogic.com>
2012-05-01 11:01:36 -0400

    Removed comment capability from XML reader (not used, so should be faster and use less memory -- well, if there are comments)

--- common/src/com/surelogic/common/xml/XMLReader.java
+++ common/src/com/surelogic/common/xml/XMLReader.java
@@ -1,36 +1,32 @@
 package com.surelogic.common.xml;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Level;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Stack;
 
-import javax.xml.parsers.*;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
-import org.xml.sax.*;
-import org.xml.sax.ext.LexicalHandler;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import com.surelogic.common.logging.SLLogger;
-
-public abstract class XMLReader extends DefaultHandler implements LexicalHandler {
+public abstract class XMLReader extends DefaultHandler {
 	public static final String PROJECT_ATTR = "project";
-	public static final String COMMENT_TAG = "<comment>";
-	
-	protected final List<String> comments = new ArrayList<String>();
+
 	protected final Stack<Entity> inside = new Stack<Entity>();
 	protected final IXMLResultListener listener;
-	
-	/**
-	 * @param l The listener that handles the top-level elements
-	 */
+
 	protected XMLReader(IXMLResultListener l) {
-		listener = l;		
+		listener = l;
 	}
-	
+
 	protected XMLReader() {
 		listener = (IXMLResultListener) this;
 	}
-	
+
 	public final void read(File location) throws Exception {
 		InputStream stream;
 		try {
@@ -40,29 +36,19 @@ public abstract class XMLReader extends DefaultHandler implements LexicalHandler
 		}
 		read(stream);
 	}
-	
+
 	public final void read(InputStream stream) throws Exception {
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		try {
 			// Parse the input
 			SAXParser saxParser = factory.newSAXParser();
-			if (this instanceof LexicalHandler) {
-				org.xml.sax.XMLReader xmlReader = saxParser.getXMLReader();
-				xmlReader.setProperty("http://xml.org/sax/properties/lexical-handler", this); 
-			}
 			saxParser.parse(stream, this);
-		/*
-		} catch (SAXException e) {
-			SLLogger.getLogger().log(Level.SEVERE, "Problem parsing JSure XML file", e);
-		} catch (ParserConfigurationException e) {
-			SLLogger.getLogger().log(Level.SEVERE, "Problem parsing JSure XML file", e);
-		*/
 		} finally {
 			listener.done();
 			stream.close();
 		}
 	}
-	
+
 	@Override
 	public final void startElement(String uri, String localName, String name,
 			Attributes attributes) throws SAXException {
@@ -72,17 +58,9 @@ public abstract class XMLReader extends DefaultHandler implements LexicalHandler
 			listener.start(uid, proj);
 			return;
 		}
-		//System.out.println("Read '"+name+"'");
-		
-		if (!inside.isEmpty()) {
-			/*
-			Entity last = inside.peek();
-			System.out.println("Started '"+name+"' inside of "+last);
-			*/
-		}
-		inside.push(listener.makeEntity(name, attributes));//new Entity(name, attributes));
+		inside.push(listener.makeEntity(name, attributes));
 	}
-	
+
 	@Override
 	public void characters(char buf[], int offset, int len) throws SAXException {
 		if (!inside.isEmpty()) {
@@ -90,16 +68,16 @@ public abstract class XMLReader extends DefaultHandler implements LexicalHandler
 			e.addToCData(buf, offset, len);
 		}
 	}
-	
+
 	@Override
 	public final void endElement(String uri, String localName, String name)
-	throws SAXException {
+			throws SAXException {
 		if (checkForRoot(name, null) != null) {
 			return;
 		}
 		Entity last = inside.pop();
 		if (!last.getName().equals(name)) {
-			System.out.println(name+" doesn't match "+last);
+			System.out.println(name + " doesn't match " + last);
 		} else {
 			if (!inside.isEmpty()) {
 				Entity next = inside.peek();
@@ -116,58 +94,19 @@ public abstract class XMLReader extends DefaultHandler implements LexicalHandler
 	 * @return non-null uid if it's the root element
 	 */
 	protected abstract String checkForRoot(String name, Attributes attributes);
-	
+
 	/**
-	 * Called to do any extra processing to relate the enclosing and nested entities
-	 * @param next The nested entity
-	 * @param last The enclosing entity
+	 * Called to do any extra processing to relate the enclosing and nested
+	 * entities
+	 * 
+	 * @param next
+	 *            The nested entity
+	 * @param last
+	 *            The enclosing entity
 	 * @param lastName
 	 */
 	protected void handleNestedEntity(Entity next, Entity last, String lastName) {
-		throw new UnsupportedOperationException("There should be no nested entities");
-	}
-
-	@Override
-	public void comment(char[] ch, int start, int length) throws SAXException {
-		final String c = new String(ch, start, length);
-		if (!inside.isEmpty()) {
-			Entity e = inside.peek();
-			Map<String,String> a = new HashMap<String,String>(1);
-			a.put(COMMENT_TAG, c);
-			e.addRef(new Entity(COMMENT_TAG, a));
-		} else {
-			comments.add(c);
-		}
-	}
-
-	@Override
-	public void endCDATA() throws SAXException {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void endDTD() throws SAXException {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void endEntity(String name) throws SAXException {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void startCDATA() throws SAXException {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void startDTD(String name, String publicId, String systemId)
-			throws SAXException {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void startEntity(String name) throws SAXException {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException(
+				"handleNestedEntity() not overridden -- There should be no nested entities");
 	}
 }
