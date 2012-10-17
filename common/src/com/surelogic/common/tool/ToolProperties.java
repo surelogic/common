cf1b3bbb8f7f3f60326205335d5efb6a9860b534
Tim Halloran <tim.halloran@surelogic.com>
2012-10-17 14:39:10 -0400

    Working on filter

--- common/src/com/surelogic/common/tool/ToolProperties.java
+++ common/src/com/surelogic/common/tool/ToolProperties.java
@@ -10,19 +10,21 @@ import java.util.regex.Pattern;
 
 import com.surelogic.NonNull;
 import com.surelogic.Nullable;
+import com.surelogic.Utility;
 import com.surelogic.common.SLUtility;
 import com.surelogic.common.logging.SLLogger;
 
 /**
  * Properties shared among the various SureLogic tools.
  */
-public class ToolProperties extends Properties {
+@Utility
+public class ToolProperties {
   private static final long serialVersionUID = 1L;
 
   public static final String PROPS_FILE = "surelogic-tools.properties";
 
-  public static final String EXCLUDE_PATH = "scan.exclude.source.folder";
-  public static final String EXCLUDED_PKGS = "scan.exclude.source.package";
+  public static final String SCAN_EXCLUDE_SOURCE_FOLDER = "scan.exclude.source.folder";
+  public static final String SCAN_EXCLUDE_SOURCE_PACKAGE = "scan.exclude.source.package";
 
   /**
    * Reads in the passed file, it is suggested that the {@link #PROPS_FILE} file
@@ -34,10 +36,10 @@ public class ToolProperties extends Properties {
    *         can't be loaded.
    */
   @Nullable
-  public static ToolProperties readFileOrNull(File properties) {
+  public static Properties readFileOrNull(File properties) {
     if (properties.exists() && properties.isFile()) {
       System.out.println("FOUND " + properties.getAbsolutePath());
-      final ToolProperties props = new ToolProperties();
+      final Properties props = new Properties();
       try {
         InputStream is = new FileInputStream(properties);
         props.load(is);
@@ -52,22 +54,46 @@ public class ToolProperties extends Properties {
     }
   }
 
-  private String[] getListProperty(String key) {
-    final String l = getProperty(key);
+  private static String[] getListProperty(@NonNull Properties props, String key) {
+    if (props == null)
+      return SLUtility.EMPTY_STRING_ARRAY;
+    final String l = props.getProperty(key);
     if (l == null) {
       return SLUtility.EMPTY_STRING_ARRAY;
     }
     return l.split("[ ,]+");
   }
 
+  /**
+   * Assuming the passed properties file is from a
+   * <tt>surelogic-tools.properties</tt> file, this method returns a list of the
+   * excluded source folders.
+   * 
+   * @param props
+   *          properties from a <tt>surelogic-tools.properties</tt> file or
+   *          {@code null}.
+   * @return a possibly empty array listing exclusions.
+   */
   @NonNull
-  public String[] getExcludedSourcePaths() {
-    return getListProperty(EXCLUDE_PATH);
+  public static String[] getExcludedSourceFolders(@Nullable Properties props) {
+    return getListProperty(props, SCAN_EXCLUDE_SOURCE_FOLDER);
   }
 
+  /**
+   * Assuming the passed properties file is from a
+   * <tt>surelogic-tools.properties</tt> file, this method returns a list of the
+   * excluded packages.
+   * <p>
+   * These could include wildcards such as <tt>*test*</tt>.
+   * 
+   * @param props
+   *          properties from a <tt>surelogic-tools.properties</tt> file or
+   *          {@code null}.
+   * @return a possibly empty array listing exclusions.
+   */
   @NonNull
-  public String[] getExcludedPackages() {
-    return getListProperty(EXCLUDED_PKGS);
+  public static String[] getExcludedPackagePatterns(@Nullable Properties props) {
+    return getListProperty(props, SCAN_EXCLUDE_SOURCE_PACKAGE);
   }
 
   public static Pattern[] makePackageMatchers(String[] patterns) {
