0f330181376f34181d213f901e2db06a4d133eec
Edwin Chan <edwin.chan@surelogic.com>
2012-10-18 11:30:43 -0400

    Changed to use SLUtility.PACKAGE_INFO

--- common/src/com/surelogic/common/AnnotationConstants.java
+++ common/src/com/surelogic/common/AnnotationConstants.java
@@ -1,7 +1,7 @@
 package com.surelogic.common;
 
 public final class AnnotationConstants {
-	public static final String PACKAGE_INFO = "package-info";
+	public static final String PACKAGE_INFO = SLUtility.PACKAGE_INFO;
 	public static final String PROMISE_PREFIX = "com.surelogic.";
 	public static final String JCIP_PREFIX = "net.jcip.annotations.";
 	public static final String VALUE_ATTR = "value";
