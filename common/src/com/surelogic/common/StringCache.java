5ae8ebd3341d7fae5e17919b9981dd325233f1f3
Tim Halloran <tim.halloran@surelogic.com>
2012-11-26 16:12:04 -0500

    Javadoc to string cache

--- common/src/com/surelogic/common/StringCache.java
+++ common/src/com/surelogic/common/StringCache.java
@@ -9,20 +9,30 @@ import com.surelogic.ThreadSafe;
 /**
  * Caches strings to attempt to alias references to the same string.
  * <p>
- * Clients should toss this instance so that the map contained  
+ * Clients should toss this instance so that the map contained
  */
 @ThreadSafe
 public class StringCache {
   private final ConcurrentMap<String, String> f_stringCache = new ConcurrentHashMap<String, String>();
 
+  /**
+   * This call tries to share, via an alias, strings if the string has been
+   * previously passed to this method. It will never change the contents of the
+   * string.
+   * 
+   * @param value
+   *          a string
+   * @return the same string, but perhaps aliased with other strings that are
+   *         the same.
+   */
   @Nullable
-  public String aliasIfPossible(@Nullable final String s) {
-    if (s == null) {
+  public String aliasIfPossible(@Nullable final String value) {
+    if (value == null)
       return null;
-    }
-    final String cached = f_stringCache.putIfAbsent(s, s);
+    
+    final String cached = f_stringCache.putIfAbsent(value, value);
     if (cached == null)
-      return s; // It got cached
+      return value; // It got cached
     else
       return cached;
   }
