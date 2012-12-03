ca3d90aaf207e6f361fea6dd91d151093ebfe9e4
Edwin Chan <edwin.chan@surelogic.com>
2012-12-03 09:39:54 -0500

    Added RecursiveIOAction

---
+++ common/src/com/surelogic/common/concurrent/RecursiveIOAction.java
@@ -0,0 +1,20 @@
+package com.surelogic.common.concurrent;
+
+import java.io.IOException;
+
+import jsr166y.RecursiveAction;
+
+public abstract class RecursiveIOAction extends RecursiveAction {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected final void compute() {
+		try {
+			compute_private();
+		} catch(IOException e) {
+			completeExceptionally(e);
+		}
+	}
+	
+	protected abstract void compute_private() throws IOException;
+}
