20166474517bcb0a05d6b6e94229ba5e9bcb4d18
Tim Halloran <tim.halloran@surelogic.com>
2012-12-07 13:26:38 -0500

    Added prototype thread image

--- common/src/com/surelogic/common/CommonImages.java
+++ common/src/com/surelogic/common/CommonImages.java
@@ -238,6 +238,7 @@ public final class CommonImages {
   public static final String IMG_SORT_DOWN = "sort_down.gif";
   public static final String IMG_SORT_UP = "sort_up.gif";
   public static final String IMG_SUGGESTIONS_WARNINGS = "suggestions_warnings.gif";
+  public static final String IMG_THREAD = "thread.gif";
   public static final String IMG_TIMEOUT_X = "timeout_x.gif";
   public static final String IMG_TRAFFIC_LIGHT_GREEN = "traffic_light_green.png";
   public static final String IMG_TRAFFIC_LIGHT_RED = "traffic_light_red.png";
