e0ab228dc273ce7bb2029426b675b36aaa7cc772
Tim Halloran <tim.halloran@surelogic.com>
2012-12-17 12:54:57 -0500

    Added documentation.

--- common/src/com/surelogic/common/jobs/SLJobTracker.java
+++ common/src/com/surelogic/common/jobs/SLJobTracker.java
@@ -18,10 +18,10 @@ import com.surelogic.common.logging.SLLogger;
  */
 public final class SLJobTracker {
 
-  public final int NOT_STARTED = -100;
+  public final int NOT_STARTED = -2;
   public final int INDETERMINATE = -1;
 
-  final AtomicInteger f_percentage = new AtomicInteger(NOT_STARTED);
+  final AtomicInteger f_percentageOrState = new AtomicInteger(NOT_STARTED);
 
   final SLProgressMonitorObserver f_monitor = new SLProgressMonitorObserver() {
     public void notifyPercentComplete(int percentage) {
@@ -33,7 +33,7 @@ public final class SLJobTracker {
         SLLogger.getLogger().log(Level.WARNING, I18N.err(297, percentage, 100));
         percentage = 100;
       }
-      f_percentage.set(percentage);
+      f_percentageOrState.set(percentage);
       for (SLProgressMonitorObserver o : f_observers)
         o.notifyPercentComplete(percentage);
     }
@@ -46,25 +46,63 @@ public final class SLJobTracker {
     job.addObserver(f_monitor);
   }
 
+  /**
+   * Gets if the tracked job has started.
+   * 
+   * @return {@code true} if the tracked job has started, {@code false} if it
+   *         has not started.
+   */
   public boolean isStarted() {
-    return f_percentage.get() != NOT_STARTED;
+    return f_percentageOrState.get() != NOT_STARTED;
   }
 
-  public int getPercentage() {
-    return f_percentage.get();
+  /**
+   * Gets the percentage of work accomplished by the tracked job or if it has
+   * not started or if it has indeterminate amount of work.
+   * 
+   * @return an integer <tt>[0, 100]</tt> indicating the percent complete of the
+   *         work or <tt>-2</tt> (constant {@link #NOT_STARTED}) or <tt>-1</tt>
+   *         (constant {@link #INDETERMINATE}). A value of <tt>0</tt> indicates
+   *         started. A value of <tt>-1</tt> indicates started but with an
+   *         indeterminate amount of work&mdash;the only other call will be
+   *         <tt>100</tt> when finished. A value of <tt>100</tt> indicates
+   *         finished. A value of <tt>-2</tt> indicates not started.
+   */
+  public int getPercentageOrState() {
+    return f_percentageOrState.get();
   }
 
+  /**
+   * Gets if the tracked job is finished.
+   * 
+   * @return {@code true} if the tracked job is finished, {@code false}
+   *         otherwise.
+   */
   public boolean isFinished() {
-    return f_percentage.get() == 100;
+    return f_percentageOrState.get() == 100;
   }
 
   final CopyOnWriteArrayList<SLProgressMonitorObserver> f_observers = new CopyOnWriteArrayList<SLProgressMonitorObserver>();
 
+  /**
+   * Adds a progress monitor observer.
+   * 
+   * @param observer
+   *          an observer of {@link SLJob} progress.
+   */
   public final void addObserver(final SLProgressMonitorObserver observer) {
     if (observer != null)
       f_observers.add(observer);
   }
 
+  /**
+   * Removes a progress monitor observer.
+   * 
+   * @param observer
+   *          an observer of {@link SLJob} progress.
+   * @return {@code true} if the list of observers contained the specified
+   *         observer.
+   */
   public final boolean removeObserver(final SLProgressMonitorObserver observer) {
     if (observer != null)
       return f_observers.remove(observer);
@@ -72,6 +110,10 @@ public final class SLJobTracker {
       return false;
   }
 
+  /**
+   * Removes all progress monitor observers. There will be no progress monitor
+   * observers after this call returns.
+   */
   public final void clearObservers() {
     f_observers.clear();
   }
