60fa6d2d15a432329f35c9fee7fc146d219a2ca4
Tim Halloran <tim.halloran@surelogic.com>
2013-01-29 16:57:24 -0500

    Fixing concurrency documentation

--- common-ui-eclipse/src/com/surelogic/common/ui/SLImages.java
+++ common-ui-eclipse/src/com/surelogic/common/ui/SLImages.java
@@ -37,6 +37,9 @@ import com.surelogic.common.ref.IJavaRef;
  * All image name refer to names in {@link CommonImages}. Pass one of the
  * <tt>SharedImages.IMG_*</tt> or <tt>DECR_*</tt> constants declared in that
  * utility.
+ * <p
+ * All of the methods in this utility should only be invoked in the context of
+ * the SWT User Interface thread.
  * 
  * @see CommonImages
  */
@@ -83,6 +86,13 @@ public final class SLImages {
     registerDisposeExecIfNeeded();
   }
 
+  /**
+   * Flags if a <tt>disposeExec</tt> {@link Runnable} needs to be setup to
+   * dispose of cached images.
+   * <p>
+   * With regard to concurrency this state is thread confined to the SWT User
+   * Interface thread.
+   */
   private static boolean f_needToRegisterDisposeExec = true;
 
   /**
