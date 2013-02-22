d781399611abd6ea14e57f0926f651e81bd9ccda
Tim Halloran <tim.halloran@surelogic.com>
2013-02-22 15:01:21 -0500

    Changed visibility

--- common-ui-eclipse/src/com/surelogic/common/ui/adhoc/views/menu/AbstractQueryMenuView.java
+++ common-ui-eclipse/src/com/surelogic/common/ui/adhoc/views/menu/AbstractQueryMenuView.java
@@ -20,92 +20,89 @@ import com.surelogic.common.ui.tooltip.ToolTip;
 
 public abstract class AbstractQueryMenuView extends ViewPart {
 
-	private QueryMenuMediator f_mediator = null;
+  private QueryMenuMediator f_mediator = null;
 
-	public abstract AdHocManager getManager();
+  public abstract AdHocManager getManager();
 
-	public ToolTip getToolTip(Shell shell) {
-		return new ToolTip(shell);
-	}
+  public ToolTip getToolTip(Shell shell) {
+    return new ToolTip(shell);
+  }
 
-	/**
-	 * Gets the message for this view to display when no database is selected to
-	 * query. Intended to be overridden by subclasses to provide a more helpful
-	 * message.
-	 * 
-	 * @return a non-null message to display when no database is selected to
-	 *         query.
-	 */
-	public String getNoDatabaseMessage() {
-		return I18N.msg("adhoc.query.menu.label.noDatabaseSelected");
-	}
+  /**
+   * Gets the message for this view to display when no database is selected to
+   * query. Intended to be overridden by subclasses to provide a more helpful
+   * message.
+   * 
+   * @return a non-null message to display when no database is selected to
+   *         query.
+   */
+  public String getNoDatabaseMessage() {
+    return I18N.msg("adhoc.query.menu.label.noDatabaseSelected");
+  }
 
-	/**
-	 * Indicates that a particular query on the database will result in no data.
-	 * Intended to be overridden by subclasses to provide an answer.
-	 * <p>
-	 * This method is used to provide visual indication in the user interface
-	 * that the query will not result in data.
-	 * <p>
-	 * The default implementation returns {@code false}.
-	 * 
-	 * @param query
-	 *            the query.
-	 * @return {@code true} if the query when run on the database will result in
-	 *         no data, {@code false} otherwise.
-	 */
-	public boolean queryResultWillBeEmpty(final AdHocQuery query) {
-		return false;
-	}
+  /**
+   * Indicates that a particular query on the database will result in no data.
+   * Intended to be overridden by subclasses to provide an answer.
+   * <p>
+   * This method is used to provide visual indication in the user interface that
+   * the query will not result in data.
+   * <p>
+   * The default implementation returns {@code false}.
+   * 
+   * @param query
+   *          the query.
+   * @return {@code true} if the query when run on the database will result in
+   *         no data, {@code false} otherwise.
+   */
+  protected boolean queryResultWillBeEmpty(final AdHocQuery query) {
+    return false;
+  }
 
-	@Override
-	public void createPartControl(final Composite parent) {
-		parent.setLayout(new FillLayout());
+  @Override
+  public void createPartControl(final Composite parent) {
+    parent.setLayout(new FillLayout());
 
-		final PageBook pageBook = new PageBook(parent, SWT.NONE);
+    final PageBook pageBook = new PageBook(parent, SWT.NONE);
 
-		final Label noRunSelected = new Label(pageBook, SWT.NONE);
+    final Label noRunSelected = new Label(pageBook, SWT.NONE);
 
-		final Table queryMenu = new Table(pageBook, SWT.BORDER
-				| SWT.FULL_SELECTION);
+    final Table queryMenu = new Table(pageBook, SWT.BORDER | SWT.FULL_SELECTION);
 
-		// init() called by the mediator
-		final QueryResultNavigator navigator = QueryResultNavigator
-				.getInstance(getManager().getDataSource());
+    // init() called by the mediator
+    final QueryResultNavigator navigator = QueryResultNavigator.getInstance(getManager().getDataSource());
 
-		final IActionBars actionBars = getViewSite().getActionBars();
+    final IActionBars actionBars = getViewSite().getActionBars();
 
-		final IToolBarManager toolBar = actionBars.getToolBarManager();
-		toolBar.add(navigator.getClearSelectionAction());
+    final IToolBarManager toolBar = actionBars.getToolBarManager();
+    toolBar.add(navigator.getClearSelectionAction());
 
-		final IMenuManager menu = actionBars.getMenuManager();
-		menu.add(navigator.getClearSelectionAction());
+    final IMenuManager menu = actionBars.getMenuManager();
+    menu.add(navigator.getClearSelectionAction());
 
-		final ToolTip tip = getToolTip(parent.getShell());
-		tip.activateToolTip(queryMenu);
+    final ToolTip tip = getToolTip(parent.getShell());
+    tip.activateToolTip(queryMenu);
 
-		f_mediator = new QueryMenuMediator(this, pageBook, noRunSelected,
-				queryMenu, navigator);
-		f_mediator.init();
-	}
+    f_mediator = new QueryMenuMediator(this, pageBook, noRunSelected, queryMenu, navigator);
+    f_mediator.init();
+  }
 
-	@Override
-	public void setFocus() {
-		if (f_mediator != null) {
-			f_mediator.setFocus();
-		}
-	}
+  @Override
+  public void setFocus() {
+    if (f_mediator != null) {
+      f_mediator.setFocus();
+    }
+  }
 
-	@Override
-	public void dispose() {
-		if (f_mediator != null) {
-			f_mediator.dispose();
-			f_mediator = null;
-		}
-		super.dispose();
-	}
+  @Override
+  public void dispose() {
+    if (f_mediator != null) {
+      f_mediator.dispose();
+      f_mediator = null;
+    }
+    super.dispose();
+  }
 
-	public void runRootQuery(final String id) {
-		f_mediator.runRootQuery(id);
-	}
+  public void runRootQuery(final String id) {
+    f_mediator.runRootQuery(id);
+  }
 }
