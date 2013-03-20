e2df2c514d7da5f508e1e9f8f8a11842401af50a
edwin <edwin.chan@surelogic.com>
2013-03-20 15:39:47 -0400

    Created AbstractLocalConfig

---
+++ common/src/com/surelogic/common/jobs/remote/AbstractLocalConfig.java
@@ -0,0 +1,37 @@
+package com.surelogic.common.jobs.remote;
+
+import java.io.File;
+import java.util.logging.Level;
+
+import com.surelogic.common.XUtil;
+import com.surelogic.common.logging.SLLogger;
+
+public abstract class AbstractLocalConfig implements ILocalConfig {
+	final int memSize;
+	final File runDir;
+
+	protected AbstractLocalConfig(int mem, File dir) {
+		memSize = mem;
+		runDir = dir;
+	}
+	
+	public final int getMemorySize() {
+		return memSize;
+	}
+
+	public String getTestCode() {
+		return TestCode.NONE.name();
+	}
+
+	public boolean isVerbose() {
+		return SLLogger.getLogger().isLoggable(Level.FINE) || XUtil.testing;
+	}
+	
+	public final String getLogPath() {
+		return new File(runDir, AbstractRemoteSLJob.LOG_NAME).getAbsolutePath();
+	}
+	
+	public final String getRunDirectory() {
+		return runDir.getAbsolutePath();
+	}
+}
