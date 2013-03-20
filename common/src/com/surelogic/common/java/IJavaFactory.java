c3c9de666f85cb612aab508d1787d15598a3da98
edwin <edwin.chan@surelogic.com>
2013-03-20 15:37:53 -0400

    Added a prototype IJavaFactory

--- common/src/com/surelogic/common/java/IJavaFactory.java
+++ common/src/com/surelogic/common/java/IJavaFactory.java
@@ -9,4 +9,15 @@ import com.surelogic.common.jobs.SLProgressMonitor;
 public interface IJavaFactory<P extends ISLJavaProject> {		
 	P newProject(JavaProjectSet<P> projects, Config config, String name, SLProgressMonitor monitor);
 	JavaProjectSet<P> newProjectSet(File loc, boolean isAuto, Date time, Map<String, Object> args);
+	
+	static final IJavaFactory<JavaProject> prototype = new IJavaFactory<JavaProject>() {
+		public JavaProject newProject(JavaProjectSet<JavaProject> projects,
+				Config config, String name, SLProgressMonitor monitor) {
+			return new JavaProject(projects, config, name, monitor);
+		}
+		public JavaProjectSet<JavaProject> newProjectSet(File loc,
+				boolean isAuto, Date time, Map<String, Object> args) {
+			return new JavaProjectSet<JavaProject>(this, loc, isAuto, time, args);
+		}
+	};
 }
