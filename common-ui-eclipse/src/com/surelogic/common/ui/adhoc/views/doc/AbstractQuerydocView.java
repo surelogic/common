3da94297273cce26c611db6ec81ec6f52d7e588d
Nathan Boy <nathan.boy@gmail.com>
2013-04-02 15:32:16 -0400

    Updated HTML Printer to set foreground as well as background, otherwise it's too easy to end up with conflicting colors.

--- common-ui-eclipse/src/com/surelogic/common/ui/adhoc/views/doc/AbstractQuerydocView.java
+++ common-ui-eclipse/src/com/surelogic/common/ui/adhoc/views/doc/AbstractQuerydocView.java
@@ -4,7 +4,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.part.ViewPart;
 
@@ -12,42 +11,36 @@ import com.surelogic.common.adhoc.AdHocManager;
 
 public abstract class AbstractQuerydocView extends ViewPart {
 
-  private QuerydocMediator f_mediator = null;
+    private QuerydocMediator f_mediator = null;
 
-  public abstract AdHocManager getManager();
+    public abstract AdHocManager getManager();
 
-  @Override
-  public void createPartControl(Composite parent) {
-    parent.setLayout(new FillLayout());
-    final Browser browser = new Browser(parent, SWT.NONE);
+    @Override
+    public void createPartControl(Composite parent) {
+        parent.setLayout(new FillLayout());
+        final Browser browser = new Browser(parent, SWT.NONE);
 
-    // tipBrowser.setJavascriptEnabled(false);
+        // Replace browser's built-in context menu with none
+        browser.setMenu(new Menu(parent.getShell(), SWT.NONE));
 
-    final Display display = parent.getDisplay();
-    browser.setForeground(display.getSystemColor(SWT.COLOR_INFO_FOREGROUND));
-    browser.setBackground(display.getSystemColor(SWT.COLOR_INFO_BACKGROUND));
-
-    // Replace browser's built-in context menu with none
-    browser.setMenu(new Menu(parent.getShell(), SWT.NONE));
-
-    f_mediator = new QuerydocMediator(this, browser);
-    f_mediator.init();
-  }
-
-  @Override
-  public void setFocus() {
-    if (f_mediator != null) {
-      f_mediator.setFocus();
+        f_mediator = new QuerydocMediator(this, browser);
+        f_mediator.init();
     }
-  }
 
-  @Override
-  public void dispose() {
-    if (f_mediator != null) {
-      f_mediator.dispose();
-      f_mediator = null;
+    @Override
+    public void setFocus() {
+        if (f_mediator != null) {
+            f_mediator.setFocus();
+        }
     }
-    super.dispose();
-  }
+
+    @Override
+    public void dispose() {
+        if (f_mediator != null) {
+            f_mediator.dispose();
+            f_mediator = null;
+        }
+        super.dispose();
+    }
 
 }
