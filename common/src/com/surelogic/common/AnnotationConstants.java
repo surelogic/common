1b874ff17867e56a4ebdc816d60adb0822c021ba
edwin <edwin.chan@surelogic.com>
2013-05-10 11:51:57 -0400

    Added code to support javax and com.android versions of Immutable and GuardedBy

--- common/src/com/surelogic/common/AnnotationConstants.java
+++ common/src/com/surelogic/common/AnnotationConstants.java
@@ -4,6 +4,7 @@ public final class AnnotationConstants {
 	public static final String PACKAGE_INFO = SLUtility.PACKAGE_INFO;
 	public static final String PROMISE_PREFIX = "com.surelogic.";
 	public static final String JCIP_PREFIX = "net.jcip.annotations.";
-	public static final String ANDROID_PREFIX = "com.android.internal.annotations.";
+	public static final String ANDROID_INTERNAL_PREFIX = "com.android.internal.annotations.";
+	public static final String JAVAX_PREFIX = "javax.annotation.concurrent.";
 	public static final String VALUE_ATTR = "value";
 }
