3c9883b81c0587c2a9c06664ad242fd8f41393a5
edwin <edwin.chan@surelogic.com>
2014-03-19 16:11:09 -0400

    Added constants to support javax.annotation

--- common/src/com/surelogic/common/AnnotationConstants.java
+++ common/src/com/surelogic/common/AnnotationConstants.java
@@ -5,6 +5,8 @@ public final class AnnotationConstants {
 	public static final String PROMISE_PREFIX = "com.surelogic.";
 	public static final String JCIP_PREFIX = "net.jcip.annotations.";
 	public static final String ANDROID_INTERNAL_PREFIX = "com.android.internal.annotations.";
-	public static final String JAVAX_PREFIX = "javax.annotation.concurrent.";
+	public static final String JAVAX_PREFIX = "javax.annotation.";
+	public static final String JAVAX_CONCURRENT_PREFIX = "javax.annotation.concurrent.";
 	public static final String VALUE_ATTR = "value";
+	public static final String JAVAX_NONNULL = JAVAX_PREFIX+"Nonnull";
 }
