593709e2a30a8e75928f207d88c7c2203c10e190
Tim Halloran <tim.halloran@surelogic.com>
2014-09-24 15:59:07 -0400

    New files

---
+++ common/src/com/surelogic/common/ref/DeclLambda.java
@@ -0,0 +1,37 @@
+package com.surelogic.common.ref;
+
+import java.util.List;
+
+import com.surelogic.Immutable;
+import com.surelogic.NonNull;
+import com.surelogic.Nullable;
+
+@Immutable
+final class DeclLambda extends Decl implements IDeclType {
+
+  final int f_declPosition;
+  final TypeRef f_functionalInterfaceType;
+
+  DeclLambda(IDecl parent, List<Decl.DeclBuilder> childBuilders, String name, int declPosition, TypeRef functionalInterfaceType) {
+    super(parent, childBuilders, name);
+    f_declPosition = declPosition;
+    f_functionalInterfaceType = functionalInterfaceType;
+  }
+
+  @Override
+  @NonNull
+  public Kind getKind() {
+    return Kind.LAMBDA;
+  }
+
+  @Override
+  public int getPosition() {
+    return f_declPosition;
+  }
+
+  @Override
+  @Nullable
+  public TypeRef getTypeOf() {
+    return f_functionalInterfaceType;
+  }
+}
---
+++ common/src/com/surelogic/common/ref/IDeclLambda.java
@@ -0,0 +1,11 @@
+package com.surelogic.common.ref;
+
+import com.surelogic.common.ref.IDecl.Kind;
+
+/**
+ * Interface for declaration instances where {@link #getKind()} ==
+ * {@link Kind#LAMBDA}.
+ */
+public interface IDeclLambda extends IDecl {
+  // marker interface
+}
