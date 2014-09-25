f92d3d7078f334771260b662afc1e2b9caafdb79
Tim Halloran <tim.halloran@surelogic.com>
2014-09-25 12:15:48 -0400

    Java 8 images (default decorator and lambda)

--- common/src/com/surelogic/common/CommonImages.java
+++ common/src/com/surelogic/common/CommonImages.java
@@ -33,6 +33,7 @@ public final class CommonImages {
   public static final String DECR_CONSISTENT = "decr/consistent.gif";
   public static final String DECR_CONSTRUCTOR = "decr/constructor.gif";
   public static final String DECR_COUNT = "decr/count.png";
+  public static final String DECR_DEFAULT = "decr/default.gif";
   public static final String DECR_DELTA = "decr/delta.gif";
   public static final String DECR_ERROR = "decr/error.gif";
   public static final String DECR_FINAL = "decr/final.gif";
@@ -185,6 +186,7 @@ public final class CommonImages {
   public static final String IMG_JSURE_QUICK_REF = "jsure_quick_ref.png";
   public static final String IMG_JSURE_RE_VERIFY = "jsure_re_verify.gif";
   public static final String IMG_JSURE_VERIFY = "jsure_verify.gif";
+  public static final String IMG_LAMBDA = "lambda.gif";
   public static final String IMG_LEFT_ARROW_SMALL = "left_arrow_small.gif";
   public static final String IMG_LEFT = "left.gif";
   public static final String IMG_LIBRARY = "library.gif";
