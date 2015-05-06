4582e13ba1a241ccdef64ad4019d42cd615768ee
Tim Halloran <tim.halloran@surelogic.com>
2015-05-06 14:46:55 -0400

    Added a separate license for Flashlight Android (which implies a license for Flashlight for Java)

--- common/src/com/surelogic/common/license/SLLicenseProduct.java
+++ common/src/com/surelogic/common/license/SLLicenseProduct.java
@@ -13,82 +13,85 @@ import com.surelogic.RegionEffects;
 @Immutable
 public enum SLLicenseProduct {
 
-	ALL_TOOLS("All Tools"),
+  ALL_TOOLS("All Tools"),
 
-	FLASHLIGHT("Flashlight"),
+  FLASHLIGHT("Flashlight"),
 
-	JSURE("JSure"),
+  FLASHLIGHT_ANDROID("Flashlight (Android)"),
 
-	SIERRA("Sierra"),
+  JSURE("JSure"),
 
-	EXEMPT("Exempt");
+  SIERRA("Sierra"),
 
-	private final String f_symbol;
+  EXEMPT("Exempt");
 
-	SLLicenseProduct(String symbol) {
-		f_symbol = symbol;
-	}
+  private final String f_symbol;
 
-	/**
-	 * Checks if this product requires a license for use.
-	 * 
-	 * @return {@code true} if a license is required to use this product,
-	 *         {@code false} otherwise.
-	 */
-	public boolean needsLicense() {
-		if (this == EXEMPT)
-			return false;
-		if (this == ALL_TOOLS)
-			return false;
-		return true;
-	}
+  SLLicenseProduct(String symbol) {
+    f_symbol = symbol;
+  }
 
-	/**
-	 * Checks if a license for the receiver product includes, or implies, a
-	 * license for the passed product.
-	 * 
-	 * @param product
-	 *            a product.
-	 * @return {@code true} if a license for the this product includes a license
-	 *         for <tt>product</tt>, {@code false} otherwise.
-	 */
-	public boolean includes(final SLLicenseProduct product) {
-		if (product == null)
-			return false;
-		if (this == product)
-			return true;
-		if (this == ALL_TOOLS)
-			return true;
-		return false;
-	}
+  /**
+   * Checks if this product requires a license for use.
+   * 
+   * @return {@code true} if a license is required to use this product,
+   *         {@code false} otherwise.
+   */
+  public boolean needsLicense() {
+    if (this == EXEMPT)
+      return false;
+    if (this == ALL_TOOLS)
+      return false;
+    return true;
+  }
 
-	@RegionEffects("reads this:Instance")
-	@Override
-	public String toString() {
-		return f_symbol;
-	}
+  /**
+   * Checks if a license for the receiver product includes, or implies, a
+   * license for the passed product.
+   * 
+   * @param product
+   *          a product.
+   * @return {@code true} if a license for the this product includes a license
+   *         for <tt>product</tt>, {@code false} otherwise.
+   */
+  public boolean includes(final SLLicenseProduct product) {
+    if (product == null)
+      return false;
+    if (this == product)
+      return true;
+    if (this == ALL_TOOLS)
+      return true;
+    if (this == FLASHLIGHT_ANDROID && product == FLASHLIGHT)
+      return true;
+    return false;
+  }
 
-	/*
-	 * See page 154 of Bloch's <i>Effective Java</i> (second edition) for a
-	 * further description of supporting a fromString operation.
-	 */
-	@Vouch("Immutable")
-	private static final Map<String, SLLicenseProduct> stringToEnum = new HashMap<String, SLLicenseProduct>();
-	static {
-		for (SLLicenseProduct type : values()) {
-			stringToEnum.put(type.toString(), type);
-		}
-	}
+  @RegionEffects("reads this:Instance")
+  @Override
+  public String toString() {
+    return f_symbol;
+  }
 
-	/**
-	 * Returns the product from its name.
-	 * 
-	 * @param value
-	 *            a product name, such as <tt>"All Tools"</tt>.
-	 * @return the product, or {@code null} if the passed value is not
-	 *         recognized.
-	 */
-	public static SLLicenseProduct fromString(String value) {
-		return stringToEnum.get(value);
-	}
+  /*
+   * See page 154 of Bloch's <i>Effective Java</i> (second edition) for a
+   * further description of supporting a fromString operation.
+   */
+  @Vouch("Immutable")
+  private static final Map<String, SLLicenseProduct> stringToEnum = new HashMap<String, SLLicenseProduct>();
+  static {
+    for (SLLicenseProduct type : values()) {
+      stringToEnum.put(type.toString(), type);
+    }
+  }
+
+  /**
+   * Returns the product from its name.
+   * 
+   * @param value
+   *          a product name, such as <tt>"All Tools"</tt>.
+   * @return the product, or {@code null} if the passed value is not recognized.
+   */
+  public static SLLicenseProduct fromString(String value) {
+    return stringToEnum.get(value);
+  }
 }
