1fd09c5c6edcd2bb74cb88ce5afa95b033f3e73d
Tim Halloran <tim.halloran@surelogic.com>
2015-08-27 16:55:02 -0400

    Working

--- surelogic-web-site/src/com/surelogic/server/serviceability/admin/HTMLQuery.java
+++ surelogic-web-site/src/com/surelogic/server/serviceability/admin/HTMLQuery.java
@@ -78,10 +78,11 @@ public abstract class HTMLQuery extends NullDBQuery {
 
   void style() {
     writer.println("<style>");
-    writer.println(" body {  background-color: #FFCC66; }");
+    writer.println(" body {  background-color: #6699FF; }");
     writer.println(" table {  border-collapse: collapse; }");
-    writer.println(" td, th { border: thin solid grey; font: 10px arial, sans-serif; }");
+    writer.println(" td, th { border: thin solid grey; font: 12px arial, sans-serif; }");
     writer.println(" p { font: 15px arial, sans-serif; }");
+    writer.println(" h1 { font: 20px arial, sans-serif; }");
     writer.println("</style>");
   }
 
