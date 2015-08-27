d35bb82f8cfe5299dbe4c397cf93c6da5783b158
Tim Halloran <tim.halloran@surelogic.com>
2015-08-27 18:02:39 -0400

    Working on paging

--- surelogic-web-site/src/com/surelogic/server/serviceability/admin/LicenseActivityLogServlet.java
+++ surelogic-web-site/src/com/surelogic/server/serviceability/admin/LicenseActivityLogServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.surelogic.Nullable;
-import com.surelogic.common.Pair;
 import com.surelogic.common.jdbc.Query;
 import com.surelogic.common.jdbc.Result;
 import com.surelogic.common.jdbc.ResultHandler;
@@ -24,7 +23,6 @@ public class LicenseActivityLogServlet extends HttpServlet {
 
   private static final long serialVersionUID = 1584106224306833877L;
   private static final String TIME = "t";
-  private static final String SKIP_COUNT = "skip";
 
   @Override
   protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
@@ -37,23 +35,20 @@ public class LicenseActivityLogServlet extends HttpServlet {
   }
 
   private void handle(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
-    ServicesDBConnection.getInstance()
-        .withReadOnly(new LogQuery(resp.getWriter(), req.getParameter(TIME), req.getParameter(SKIP_COUNT)));
+    ServicesDBConnection.getInstance().withReadOnly(new LogQuery(resp.getWriter(), req.getParameter(TIME)));
   }
 
   static class LogQuery extends HTMLQuery {
 
     final long time;
-    long skipThisTime;
 
-    public LogQuery(final PrintWriter writer, @Nullable final String time, @Nullable final String skipCount) {
+    public LogQuery(final PrintWriter writer, @Nullable final String time) {
       super(writer);
       if (time == null) {
         this.time = System.currentTimeMillis();
       } else {
         this.time = Long.parseLong(time);
       }
-      this.skipThisTime = skipCount == null ? 0 : Long.parseLong(skipCount);
     }
 
     @Override
@@ -62,37 +57,30 @@ public class LicenseActivityLogServlet extends HttpServlet {
       tableBegin();
       tableRow(DATE.th("Date"), STRING.th("IP"), STRING.th("License"), STRING.th("Event"), STRING.th("Holder"), STRING.th("Email"),
           STRING.th("Company"));
-      final Pair<Long, Long> result = q.prepared("WebServices.selectNetChecksBefore", new ResultHandler<Pair<Long, Long>>() {
+      final long latest = q.prepared("WebServices.selectNetChecksBefore", new ResultHandler<Long>() {
         @Override
-        public Pair<Long, Long> handle(final Result result) {
+        public Long handle(final Result result) {
           long latest = time;
-          long skipCountNextTime = 0;
           int count = 0;
           for (Row r : result) {
-            if (skipThisTime-- > 0)
-              continue;
-            if (++count > ROWS) {
-              break;
-            }
+            count++;
             Timestamp t = r.nextTimestamp();
             final long tTime = t.getTime();
-            if (tTime == latest)
-              skipCountNextTime++;
-            else {
+            // continue output until we see a distinct time
+            if (latest != tTime) {
               latest = tTime;
-              skipCountNextTime = 0; // reset
+              if (count > ROWS) {
+                break;
+              }
             }
             tableRow(DATE.td(t), STRING.td(ip(r.nextString())), STRING.td(uuid(r.nextString())), STRING.td(r.nextString()),
                 STRING.td(r.nextString()), STRING.td(r.nextString()), STRING.td(r.nextString()));
           }
-          return new Pair<>(latest, skipCountNextTime);
+          return latest;
         }
       }).call(new Timestamp(time));
-      final long latest = result.first();
-      final long skipCount = result.second();
-      System.out.println("LATEST TO USE IN LINK IS " + latest);
       tableRow(STRING.td(""), STRING.td(""), STRING.td(""), STRING.td(""), STRING.td(""), STRING.td(""),
-          STRING.td("<a href=\"log?%s=%d&%s=%d\">Next</a>", TIME, latest, SKIP_COUNT, skipCount));
+          STRING.td("<a href=\"log?%s=%d\">Next</a>", TIME, latest));
       tableEnd();
       finish();
     }
