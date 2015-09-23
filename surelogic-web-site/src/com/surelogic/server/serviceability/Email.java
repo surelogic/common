f9ab020ae48bdb132e7e57d284416a1856796d67
Tim Halloran <tim.halloran@surelogic.com>
2015-09-23 12:11:27 -0400

    Working on website email

--- surelogic-web-site/src/com/surelogic/server/serviceability/Email.java
+++ surelogic-web-site/src/com/surelogic/server/serviceability/Email.java
@@ -7,11 +7,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 
-import javax.mail.Address;
-import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -19,18 +16,16 @@ import javax.mail.internet.MimeMessage;
 
 import com.surelogic.NonNull;
 import com.surelogic.Nullable;
+import com.surelogic.common.SLUtility;
+import com.surelogic.common.i18n.I18N;
 import com.surelogic.common.logging.SLLogger;
 
 public class Email {
 
   static final AtomicReference<ExecutorService> executor = new AtomicReference<>(null);
-  static final AtomicReference<String> emailAs = new AtomicReference<>(null);
-  static final AtomicReference<String> emailSecret = new AtomicReference<>(null);
 
-  static void start(String as, String secret) {
+  static void start() {
     executor.set(Executors.newSingleThreadExecutor());
-    emailAs.set(as);
-    emailSecret.set(secret);
   }
 
   static void stop() {
@@ -70,63 +65,37 @@ public class Email {
    * 
    * @param subject
    *          the subject for the email
-   * @param content
+   * @param msgBody
    *          the content of the request
    * @param to
-   *          the email address to send the message to or
-   *          <tt>support@surelogic.com</tt> if null.
+   *          the email address to send the message to&mdash;if null the value
+   *          of {@link SLUtility#SERVICEABILITY_EMAIL} is used.
    * @param replyTo
-   *          the email address to reply to, this is ignored if the address
-   *          invalid or null.
+   *          the email address to send a reply to&mdash;if null the value of
+   *          {@link SLUtility#SERVICEABILITY_EMAIL} is used.
    */
-  static void sendEmail(@NonNull final String subject, @NonNull final String content, @Nullable final String to,
-      @Nullable final String replyTo) {
-    final String actualTo = to != null ? to : "support@surelogic.com";
+  static void sendEmail(@NonNull final String subject, @NonNull final String msgBody, @Nullable String to,
+      @Nullable String replyTo) {
+    if (subject == null)
+      throw new IllegalArgumentException(I18N.err(44, "subject"));
+    if (msgBody == null)
+      throw new IllegalArgumentException(I18N.err(44, "msgBody"));
+    final String actualTo = to != null ? to : SLUtility.SERVICEABILITY_EMAIL;
+    final String actualReplyTo = replyTo != null ? replyTo : SLUtility.SERVICEABILITY_EMAIL;
     executor.get().execute(new Runnable() {
       @Override
       public void run() {
         try {
           final Properties props = new Properties();
-          props.setProperty("mail.transport.protocol", "smtp");
-          props.setProperty("mail.smtp.host", "smtp.gmail.com");
-          props.put("mail.smtp.port", 587);
-          props.setProperty("mail.smtp.starttls.enable", "true");
-          props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-          props.put("mail.smtp.auth", "true");
-          Authenticator auth;
-          final String from = emailAs.get();
-          final String secret = emailSecret.get();
-          if ((from != null) && (from.length() > 0)) {
-            auth = new Authenticator() {
-              @Override
-              protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(from, secret);
-              }
-
-            };
-          } else {
-            auth = null;
-          }
-          final Session session = Session.getInstance(props, auth);
+          final Session session = Session.getInstance(props, null);
 
           // create and populate a JavaMail email
           final MimeMessage msg = new MimeMessage(session);
-          msg.setFrom(new InternetAddress(from));
+          msg.setFrom(new InternetAddress(actualReplyTo));
           msg.setRecipient(Message.RecipientType.TO, new InternetAddress(actualTo));
           msg.setSubject(subject);
           msg.setSentDate(new Date());
-          msg.setContent(content, "text/plain");
-
-          // attempt to setup the reply to
-          if (replyTo != null) {
-            try {
-              final Address[] rt = InternetAddress.parse(replyTo);
-              msg.setReplyTo(rt);
-            } catch (Exception ignore) {
-              // just ignore, probably the user gave us a bad email address
-            }
-          }
-
+          msg.setContent(msgBody, "text/plain");
           // transmit the email
           Transport.send(msg);
         } catch (final MessagingException me) {
